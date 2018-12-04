2193c17158252f20fd0285ed85f484628e983771
romellem <romellem.git@gmail.com>
2018-12-04 10:26:16 -0600

    Mostly working code for part one
    Woof, this is all over the place, will come back for a refactor later if
    I care enough.

--- 4/company.js
+++ 4/company.js
@@ -6,11 +6,22 @@ class Company {
         this.log = this.sortLog(unsorted_log);
         this.guards = {};
 
+        this.parseLog();
+        let guard_most_asleep = this.getGuardMostAsleep();
+        let time_when_guard_is_asleep_most = guard_most_asleep.getMinuteMostLikelyToBeAsleepDuring();
 
+        console.log(`Guard most asleep is ${guard_most_asleep.id}`);
+        console.log(`Sleeps the most during minute ${time_when_guard_is_asleep_most}`);
+        console.log(
+            `ID * minutes = ${
+                guard_most_asleep.id
+            } * ${time_when_guard_is_asleep_most} = ${guard_most_asleep.id *
+                time_when_guard_is_asleep_most}`
+        );
     }
 
     /**
-     * @param {Array} unsorted_log 
+     * @param {Array} unsorted_log
      * @returns {Array} Returns a sorted log
      */
     sortLog(unsorted_log) {
@@ -45,14 +56,40 @@ class Company {
         return log;
     }
 
-    // parseLog() {
-    //     this.log.forEach(line => {
-    //         if (line.id && )
-    //         if (line.action === FALLS_ASLEEP)
-    //     })
-    // }
+    parseLog() {
+        this.log.forEach(shift => {
+            let { id } = shift[0];
+            if (!this.guards[id]) {
+                this.guards[id] = new Guard(id);
+            }
 
+            for (let i = 1; i < shift.length; i += 2) {
+                let falls_asleep = shift[i].date;
+                let wakes_up = shift[i + 1].date;
 
+                this.guards[id].addSleeping(falls_asleep, wakes_up);
+            }
+        });
+    }
+
+    getGuardMostAsleep() {
+        let guards = Object.values(this.guards);
+
+        let guard_most_asleep = {
+            id: undefined,
+            time: -1,
+        };
+        guards.forEach(guard => {
+            let total_asleep = guard.getTotalTimeAsleep();
+            if (total_asleep > guard_most_asleep.time) {
+                guard_most_asleep.time = total_asleep;
+                guard_most_asleep.id = guard.id;
+            }
+        });
+
+        console.log(guard_most_asleep);
+        return this.guards[guard_most_asleep.id];
+    }
 }
 
 module.exports = Company;
