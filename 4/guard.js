b147bfe0e02458eb97ffccf6712f6290498a6462
romellem <romellem.git@gmail.com>
2018-12-04 10:26:09 -0600

    Guard class

---
+++ 4/guard.js
@@ -0,0 +1,50 @@
+class Guard {
+    constructor(id) {
+        this.id = id;
+
+        this.sleeping = [];
+    }
+
+    addSleeping(falls_asleep, wakes_up) {
+        this.sleeping.push([falls_asleep.getMinutes(), wakes_up.getMinutes()]);
+    }
+
+    getTotalTimeAsleep() {
+        return this.sleeping.reduce((a, b) => {
+            return b[1] - b[0] + a;
+        }, 0);
+    }
+
+    /**
+     * @returns {Array}
+     */
+    getMinuteMostLikelyToBeAsleepDuring() {
+        let times_asleep = {};
+        for (let i = 0; i < 60; i++) {
+            times_asleep[i] = 0;
+        }
+
+        this.sleeping.forEach(nap => {
+            let [falls_asleep, wakes_up] = nap;
+            for (let i = falls_asleep; i < wakes_up; i++) {
+                times_asleep[i] += 1;
+            }
+        });
+
+        let sleep_sorted = Object.entries(times_asleep);
+        sleep_sorted.sort((a, b) => {
+            if (a[1] < b[1]) {
+                return -1;
+            } else if (a[1] > b[1]) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        let [time_most_asleep] = sleep_sorted.pop();
+        return time_most_asleep;
+    }
+}
+
+module.exports = Guard;
