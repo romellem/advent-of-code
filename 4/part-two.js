e37fa5c97c0eb0efdd1889c6e136263b6690efe7
romellem <romellem.git@gmail.com>
2018-12-04 10:32:16 -0600

    Inits part 2 code

---
+++ 4/part-two.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const Company = require('./modules/company');
+
+let raw_input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8');
+
+// Last filter is to remove any empty lines
+let input = raw_input.split('\n').filter(n => n);
+
+let c = new Company(input);
+c.outputPartTwo();
