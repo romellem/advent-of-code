4cdfe3eaafea97d72678e6841b363e0a54c3e071
romellem <romellem.git@gmail.com>
2018-12-07 12:44:28 -0600

    Part 2, and dynamic part one
    Allows for an arg to be passed in, to select a different input (in this
    case, the sample input).

--- 7/part-two.js
+++ 7/part-two.js
@@ -9,7 +9,10 @@ let raw_input = fs.readFileSync(path.resolve(__dirname, input_text_file), 'utf8'
 // Last filter is to remove any empty lines
 let input = raw_input.split('\n').filter(n => n);
 
-let tree = new Tree(input);
+const WORKERS = typeof process.argv[3] === 'undefined' ? 5 : parseInt(process.argv[3]);
+const MIN_TIME = typeof process.argv[4] === 'undefined' ? 60 : parseInt(process.argv[4]);
 
-let order = tree.generateOrder();
-console.log(`Order of our "flow chart" is: ${order}`);
+let tree = new Tree(input);
+let time = tree.calculateTime(WORKERS, MIN_TIME);
+
+console.log(`Total time (when using ${WORKERS} works and a minimum of ${MIN_TIME} seconds for each task) is: ${time}`);
