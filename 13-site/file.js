6200199d3026c529afc6c5e92f65a9b566f1f4ee
romellem <romellem.git@gmail.com>
2018-12-13 19:18:37 -0600

    A dummy site that I used to create the visualization

---
+++ 13-site/file.js
@@ -0,0 +1,26 @@
+{
+  "name": "advent-of-code-2018",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/romellem/advent-of-code-2018.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "MIT",
+  "dependencies": {
+    "array.prototype.flat": "^1.2.1",
+    "colors": "^1.3.3",
+    "concurrently": "^4.1.0",
+    "jimp": "^0.6.0",
+    "lodash": "^4.17.11",
+    "manhattan": "^1.0.0",
+    "rimraf": "^2.6.2",
+    "table": "^5.1.1"
+  }
+}
