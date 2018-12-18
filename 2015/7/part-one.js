949e4da7b16f2899351ee03a28ad1a541a9e6500
romellem <romellem.git@gmail.com>
2018-12-18 17:49:20 -0600

    Saves progress...

---
+++ 2015/7/part-one.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+let raw_input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8');
+
+// Last filter is to remove any empty lines
+let input = raw_input.split('\n').filter(n => n);
