9b9df542b88b256998242e449ca5e776a8c36867
romellem <romellem.git@gmail.com>
2018-12-22 00:08:47 -0600

    Saves progress...

--- 2018/22/cave.js
+++ 2018/22/cave.js
@@ -1,9 +1,18 @@
+const ROCKY = 0;
+const WET = 1;
+const NARROW = 2;
+
+const distance = require('manhattan');
 class Cave {
     constructor(depth, target_coords) {
-
-        // let grid_size = Math.max.apply(null, target_coords);
-        // grid_size = grid_size * grid_size;
-        let grid_size = target_coords[0] + target_coords[1] + 2;
+        /**
+         * Used the below code for Part One, due to memory issues, but that can be fixed with
+         * 
+         *     node --max_old_space_size=4096 ./part-one.js`
+         * 
+         */
+        // let grid_size = target_coords[0] + target_coords[1] + 2;
+        let grid_size = depth;
 
         this.depth = depth;
         this.grid = Array(grid_size)
@@ -72,6 +81,72 @@ class Cave {
         }
 
         return sum;
+    }
+
+
+    walkToTarget() {
+        let x = 0, y = 0;
+        let equipped = 'torch';
+        let current_cell;
+
+        const target_coords = [this.target_x, this.target_y]
+
+        while (x !== this.target_x && y !== this.target_y) {
+            current_cell = this.grid[y][x];
+
+            let left =  x > 0 ? [x - 1, y] : null,
+                right = x < this.grid.length - 1 ? [x + 1, y] : null,
+                up = y > 0 ? [x, y - 1] : null,
+                down = y < this.grid.length - 1 ? [x, y + 1] : null;
+            
+            
+            let movements = [left, right, up, down].filter(coords => {
+                if (!coords) {
+                    return false;
+                }
+
+                // Otherwise, see if we can move to the cell given our current equipment
+                // e.g., For example, if you are in a rocky region, you can switch from
+                //       the torch to the climbing gear, but you cannot switch to neither.
+
+                let [to_x, to_y] = coords;
+                let move_to_cell = this.grid[to_y][to_x];
+                if (current_cell.type === ROCKY) {
+                    if ()
+                }
+            })
+            // .map(coords => {
+            //     return {
+            //         coords,
+            //         distance: distance(coords, target_coords)
+            //     }
+            // })
+            .map(coords => {
+                let [to_x, to_y] = coords;
+                let value;
+                if (equipped === 'torch') {
+                    // move between rocky (0) and narrow (2) regions
+                    let move_to_cell = this.grid[to_y][to_x];
+
+                    if (move_to_cell.type === WET) {
+
+                    }
+                } else if (equipped === 'gear') {
+                    // move between rocky (0) and wet (1)
+                } else { // Neither equipped,
+                    // move between wet (1) and narrow (2)
+                }
+
+                return {
+                    coords: info.coords,
+                    distance: info.distance,
+                    value: 0,
+                }
+            })
+
+
+            
+        }
     }
 }
 
