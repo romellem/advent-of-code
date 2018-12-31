156459370641e09c7a88df15823e2312a349fece
romellem <romellem.git@gmail.com>
2018-12-31 15:32:51 -0600

    More not working code
    I see what is wrong here, as soon as it finished the first call, it
    stops. Instead, I need to generate a list of functions to call, and run
    through that stack. When I get to the end, I won't add any more to the
    stack.
    
    I think that'll work...

--- 2015/19/part-two.js
+++ 2015/19/part-two.js
@@ -1,12 +1,9 @@
-'use strict';
 const { sampleInput, splitMolecule } = require('./input');
 let { rules, initialState } = sampleInput;
-initialState = 'HOHOHO'.split('');
+// initialState = 'HOHOHO'.split('');
 
 // const { input, splitMolecule } = require('./input');
 // const { rules, initialState } = input;
-
-let valid_solutions_length = {};
 const trampoline = fn => (...args) => {
     let result = fn(...args);
 
@@ -17,7 +14,10 @@ const trampoline = fn => (...args) => {
     return result;
 };
 
-const growMoleculesFromSeedRecursive = (seed = ['e'], step = 0) => {
+let valid_solutions_length = {};
+
+// const growMoleculesFromSeedRec = (seed = ['e'], step = 0) => {
+const growMoleculesFromSeedRec = (seed, step) => {
     // If we are at the max length, test our growth to break out of our recursive loop
     if (seed.length === initialState.length) {
         if (seed.join('') === initialState.join('')) {
@@ -28,20 +28,22 @@ const growMoleculesFromSeedRecursive = (seed = ['e'], step = 0) => {
         for (let i = 0; i < seed.length; i++) {
             let element = seed[i];
             if (rules[element]) {
-                for (let e = 0; e < rules[element].length; e++) {
-                    let grow_to = rules[element][e];
+                let paths = rules[element];
+                for (let e = 0; e < paths.length; e++) {
+                    let grow_to = paths[e];
                     let growth = seed.slice(0);
                     growth[i] = grow_to;
                     growth = splitMolecule(growth.join(''));
-                    return () => growMoleculesFromSeedRecursive(growth, step + 1);
+                    return () => growMoleculesFromSeedRec(growth, step + 1);
                 }
             }
         }
     }
 };
 
-const growMoleculesFromSeed = trampoline(growMoleculesFromSeedRecursive);
-growMoleculesFromSeed();
-let valid_solutions = Object.keys(valid_solutions_length);
+const growMoleculesFromSeed = trampoline(growMoleculesFromSeedRec);
+growMoleculesFromSeed(['e'], 0);
+
+let valid_solutions = Object.keys(valid_solutions_length).map(n => +n);
 valid_solutions.sort((a, b) => a - b);
 console.log(valid_solutions[0]);
