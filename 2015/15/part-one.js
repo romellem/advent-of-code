bb318f3bb9f818a89a451ff7a2c14a0b26614db3
romellem <romellem.git@gmail.com>
2018-12-30 20:48:47 -0600

    This one does work, but I don't get it
    I'm being stupid about the inner loop of permutations. I want my
    perm_of_amounts array to be in increasing order, which amounts_range
    already is in. Shouldn't I create my amounts first and permute that?
    Or, just do the original combintations. Won't I get [0, 0, 0, 100] and
    [100, 0, 0, 0] without having to permute them?
    
    I'm confused, but I get the right answer... Need to debug this a bit to
    see what is going on.

--- 2015/15/part-one.js
+++ 2015/15/part-one.js
@@ -16,9 +16,9 @@ const NUMBER_OF_INGREDIENTS = input.length;
  * OK, this is a bit confusing, but I remember this back in my college stats class.
  * Essentially, we use a trick of combinations [e.g. (k choose n)], to help us
  * iterate all the possible ways we can split a number into finite groups.
- *
+ * 
  * Let's simplify it: pretend we have 10 total teaspoons to work with, and 3 ingredients.
- * How many different ways...
+ * How many different ways
  */
 
 let ingredients = range(TOTAL_TEASPOONS + NUMBER_OF_INGREDIENTS);
@@ -27,24 +27,24 @@ let amounts_range, perm_of_amounts;
 let best_recipe_score = -1;
 
 for (amounts_range of G.combination(ingredients, NUMBER_OF_INGREDIENTS - 1)) {
-    /**
-     * Eh, this should be a loop but I'm being lazy, I know we have four ingredients
-     */
+    for (perm_of_amounts of G.permutation(amounts_range)) {
+        // Eh, this should be a loop but I'm being lazy, I know we have four ingredients
+        let amounts = [
+            perm_of_amounts[0],
+            perm_of_amounts[1] - perm_of_amounts[0] - 1,
+            perm_of_amounts[2] - perm_of_amounts[1] - 1,
+            ingredients.length - perm_of_amounts[2] - 2,
+        ];
 
-    // prettier-ignore
-    let amounts = [
-        (amounts_range[0])       - (0),
-        (amounts_range[1])       - (amounts_range[0] - 1),
-        (amounts_range[2])       - (amounts_range[1] - 1),
-        (ingredients.length - 1) - (amounts_range[2] - 1),
-    ];
-    let total_score = ['capacity', 'durability', 'flavor', 'texture']
-        .map(quality => input.map((v, i) => v[quality] * amounts[i]).reduce((a, b) => a + b))
-        .map(v => (v < 0 ? 0 : v))
-        .reduce((a, b) => a * b);
+        // Part one ignores calories
+        let total_score = ['capacity', 'durability', 'flavor', 'texture']
+            .map(quality => input.map((v, i) => v[quality] * amounts[i]).reduce((a, b) => a + b))
+            .map(v => (v < 0 ? 0 : v))
+            .reduce((a, b) => a * b);
 
-    if (total_score > best_recipe_score) {
-        best_recipe_score = total_score;
+        if (total_score > best_recipe_score) {
+            best_recipe_score = total_score;
+        }
     }
 }
 
