cfb1b00baedf676ff79d2549464dfb4c8c2cf269
romellem <romellem.git@gmail.com>
2018-12-31 15:20:42 -0600

    Doesn't work with sample data...

--- 2015/19/part-two.js
+++ 2015/19/part-two.js
@@ -1,20 +1,28 @@
 'use strict';
-// const { sampleInput, splitMolecule } = require('./input');
-// let { rules, initialState } = sampleInput;
-// initialState = 'HOHOHO'.split('');
+const { sampleInput, splitMolecule } = require('./input');
+let { rules, initialState } = sampleInput;
+initialState = 'HOHOHO'.split('');
 
-const { input, splitMolecule } = require('./input');
-const { rules, initialState } = input;
-
-
+// const { input, splitMolecule } = require('./input');
+// const { rules, initialState } = input;
 
 let valid_solutions_length = {};
-const growMoleculesFromSeed = (seed = ['e'], step = 0) => {
+const trampoline = fn => (...args) => {
+    let result = fn(...args);
+
+    while (typeof result === 'function') {
+        result = result();
+    }
+
+    return result;
+};
+
+const growMoleculesFromSeedRecursive = (seed = ['e'], step = 0) => {
     // If we are at the max length, test our growth to break out of our recursive loop
     if (seed.length === initialState.length) {
         if (seed.join('') === initialState.join('')) {
             // We only care about how many steps, not what those steps were
-            valid_solutions_length[step] = true
+            valid_solutions_length[step] = true;
         }
     } else {
         for (let i = 0; i < seed.length; i++) {
@@ -25,14 +33,15 @@ const growMoleculesFromSeed = (seed = ['e'], step = 0) => {
                     let growth = seed.slice(0);
                     growth[i] = grow_to;
                     growth = splitMolecule(growth.join(''));
-                    growMoleculesFromSeed(growth, step + 1)
+                    return () => growMoleculesFromSeedRecursive(growth, step + 1);
                 }
             }
         }
     }
 };
 
+const growMoleculesFromSeed = trampoline(growMoleculesFromSeedRecursive);
 growMoleculesFromSeed();
-let valid_solutions = Object.keys(valid_solutions);
+let valid_solutions = Object.keys(valid_solutions_length);
 valid_solutions.sort((a, b) => a - b);
 console.log(valid_solutions[0]);
