80612477002e650d9490d446bc2243d3cc6835ba
romellem <romellem.git@gmail.com>
2019-01-01 14:12:20 -0600

    First stab at part two, doesn't work
    I can't just take the number and add fifty, because that isn't right.
    For instance, elf #2 will visit house 100 on his fiftith stop. Ah, it
    just clicked: I need to take the elf number (the divisor) and multiply
    that by fifty, and see if that is less than the house.

---
+++ 2015/20/part-two.js
@@ -0,0 +1,17 @@
+const MINIMUM_PRESENTS = require('./input');
+const getDivisors = require('get-divisors/dist/getDivisors');
+
+let house = 0;
+let presents;
+do {
+    ++house;
+
+    let divisors = getDivisors(house);
+    let divisors_within_50_of_house = divisors.filter(v => (v + 50 > house));
+
+    let sum_of_divisors = divisors_within_50_of_house.reduce((a, b) => a + b);
+    presents = sum_of_divisors * 11;
+
+} while (presents < MINIMUM_PRESENTS);
+
+console.log(house);
