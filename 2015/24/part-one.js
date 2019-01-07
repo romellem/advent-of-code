a7044b62a01e2072875b7ed16b5ef6d1fcc888f0
romellem <romellem.git@gmail.com>
2019-01-06 21:30:55 -0600

    More optimizations.... doesn't work for real data

--- 2015/24/part-one.js
+++ 2015/24/part-one.js
@@ -2,6 +2,11 @@ let { difference } = require('lodash');
 let { input, sampleInput } = require('./input');
 const G = require('generatorics');
 
+// let g1_num = process.argv[2] || '1';
+// g1_num = parseInt(g1_num);
+
+// console.log('Using ' + g1_num);
+
 let presents = sampleInput;
 
 const sum = (a, b) => a + b;
@@ -10,11 +15,16 @@ const solutions = [];
 
 let g1, g2, g3;
 for (let g1_num = 1; g1_num <= presents.length - 2; g1_num++) {
-    for (g1 of G.combination(presents, g1_num)) {
-        let remaining_presents1 = difference(presents, g1);
+for (g1 of G.combination(presents, g1_num)) {
+    // console.log(g1);
+    let remaining_presents1 = difference(presents, g1);
+    let g1_weight = g1.reduce(sum);
 
-        for (let g2_num = 1; g2_num <= remaining_presents1.length - 1; g2_num++) {
-            for (g2 of G.combination(remaining_presents1, g2_num)) {
+    for (let g2_num = 1; g2_num <= remaining_presents1.length - 1; g2_num++) {
+        for (g2 of G.combination(remaining_presents1, g2_num)) {
+            let g2_weight = g2.reduce(sum);
+
+            if (g1_weight === g2_weight) {
                 let g3 = difference(remaining_presents1, g2);
 
                 let g1_weight = g1.reduce(sum);
@@ -34,11 +44,15 @@ for (let g1_num = 1; g1_num <= presents.length - 2; g1_num++) {
                         g1_size: g1.length,
                     };
 
+                    // console.log('Found sol ' + JSON.stringify(sol));
+                    // console.log(sol.qe + '\n===========\n\n');
+
                     solutions.push(sol);
                 }
             }
         }
     }
+}
 }
 
 solutions.sort((a, b) => {
