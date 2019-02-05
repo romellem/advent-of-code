d1c475c605bb1fc16fca8eeba8450ee0a088e134
romellem <romellem.git@gmail.com>
2019-02-05 12:31:21 -0600

    Working day 8

--- 2016/8/README.md
+++ 2016/8/README.md
@@ -1,8 +1,8 @@
 # Answers
 
-| Part 1 | Part 2 |
-|--------|--------|
-|  ` ` |  ` ` |
+| Part 1 |    Part 2    |
+|--------|--------------|
+|  `119` | `ZFHFSFOGPO` |
 
 ## --- Day 8: Two-Factor Authentication ---
 
@@ -57,50 +57,10 @@ You notice that the screen is only capable of displaying capital letters; in the
 
 After you swipe your card, what code should is the screen trying to display?
 
+-----------------
+
 ## --- Part Two ---
 
-As the door slides open, you are presented with a second door that uses a slightly
+You notice that the screen is only capable of displaying capital letters; in the font it uses, each letter is `5` pixels wide and `6` tall.
 
-more inspired security mechanism. Clearly unimpressed by the last version (in what
-
-movie is the password decrypted in order?!), the Easter Bunny engineers have worked
-
-out a better solution.
-
-
-
-Instead of simply filling in the password from left to right, the hash now also indicates
-
-the position within the password to fill. You still look for hashes that begin with five
-
-zeroes; however, now, the sixth character represents the position (0-7), and the
-
-seventh character is the character to put in that position.
-
-
-A hash result of 000001f means that f is the second character in the password. Use only the first result for each position, and ignore invalid positions.
-
-
-For example, if the Door ID is abc:
-
-
-The first interesting hash is from abc3231929, which produces 0000015...; so,
-
-5 goes in position 1: _5______.
-
-In the previous method, 5017308 produced an interesting hash; however, it is ignored,
-
-because it specifies an invalid position (8).
-
-The second interesting hash is at index 5357525, which produces 000004e...; so, e goes
-
-in position 4: _5__e___.
-
-You almost choke on your popcorn as the final character falls into place, producing the
-
-password 05ace8e3.
-
-
-Given the actual Door ID and this new method, what is the password? Be extra proud
-
-of your solution if it uses a cinematic "decrypting" animation.
+After you swipe your card, _what code is the screen trying to display?_
--- 2016/8/screen.js
+++ 2016/8/screen.js
@@ -1,14 +1,64 @@
 class Screen {
     constructor({ instructions, screenSize }) {
-
+        this.instructions = JSON.parse(JSON.stringify(instructions));
+        this.screen = Array(screenSize[1])
+            .fill()
+            .map(row => Array(screenSize[0]).fill(0));
     }
 
-    run() {
+    run(debug = false) {
+        if (debug) this.printScreen(0);
+        this.instructions.forEach((step, i) => {
+            let { type, size, row, col, by } = step;
 
+            if (type === 'rect') {
+                // Draw a rectangle in the upper left corner
+                let [size_x, size_y] = size;
+                for (let y = 0; y < size_y; y++)
+                    for (let x = 0; x < size_x; x++) this.screen[y][x] = 1;
+            } else if (type === 'shift_row') {
+                // Copy row
+                let row_copy = this.screen[row].slice(0);
+
+                by %= row_copy.length;
+                let new_end = row_copy.slice(0, row_copy.length - by);
+                let new_start = row_copy.slice(row_copy.length - by);
+                this.screen[row] = new_start.concat(new_end);
+            } else if (type === 'shift_col') {
+                // This one is trickier. Create a temp array that represents our row.
+                // Then, shift the same way, but loop through array and set the values.
+                
+                let col_copy = [];
+                for (let y = 0; y < this.screen.length; y++) {
+                    col_copy.push(this.screen[y][col]);
+                }
+
+                by %= col_copy.length;
+                let new_end = col_copy.slice(0, col_copy.length - by);
+                let new_start = col_copy.slice(col_copy.length - by);
+                let new_col = new_start.concat(new_end);
+
+                for (let y = 0; y < this.screen.length; y++) {
+                    this.screen[y][col] = new_col[y];
+                }
+            }
+
+            if (debug) {
+                this.printScreen(i + 1);
+                console.log(step)
+            }
+        });
     }
 
     countLitPixels() {
+        return this.screen.map(row => row.reduce((a, b) => a + b, 0)).reduce((a, b) => a + b, 0);
+    }
 
+    printScreen(i) {
+        let screen_str = this.screen.map(row => row.map(cell => cell ? '#' : '.').join('')).join('\n');
+        if (i == null) console.log('\n\n======================')
+        else console.log(`\n\n== ${i} =================`)
+        console.log(screen_str);
     }
 }
 
