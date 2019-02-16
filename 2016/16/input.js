3ea130bf852662633d52254866005c8720c92388
romellem <romellem.git@gmail.com>
2019-02-16 15:22:30 -0600

    Working part one

---
+++ 2016/16/input.js
@@ -0,0 +1,11 @@
+module.exports = {
+    sampleInputs: [
+        { value: '110010110100', size: 12, checksum: '100' },
+        { value: '10000', size: 20, checksum: '01100' },
+    ],
+
+    partOneInput: {
+        value: '10011111011011001',
+        size: 272,
+    },
+};
