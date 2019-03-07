ad69eebd97f95019a602cf76fef8cd0b8b32b999
romellem <romellem.git@gmail.com>
2019-03-06 20:44:28 -0600

    Working part two

--- 2016/14/input.js
+++ 2016/14/input.js
@@ -2,6 +2,7 @@ module.exports = {
     testInput: {
         salt: 'abc',
         lastIndex: 22728,
+        lastIndexStretch: 22551,
     },
     input: `ihaygndm`
 };
--- 2016/14/key-generator.js
+++ 2016/14/key-generator.js
@@ -3,28 +3,14 @@ const crypto = require('crypto');
 const THREE_CHARS_REGEX = /(\w)\1\1/;
 
 class KeyGenerator {
-    constructor(salt) {
+    constructor(salt, use_stretch_hash = false) {
         this.salt = salt;
         this.index = 0;
 
+        this.use_stretch_hash = use_stretch_hash;
+
         this.keys = [];
         this.hashCache = {};
-    }
-
-    testHashForValidity(match) {
-        // Take our 3 char match and convert it to a 5 char match
-        match = match + match[0] + match[0];
-
-        for (let i = 0; i < 1000; i++) {
-            let input = `${this.salt}${this.index + i}`;
-            let hash = this.md5(input);
-
-            if (hash.includes(match)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     static MD5(str) {
@@ -32,6 +18,16 @@ class KeyGenerator {
             .createHash('md5')
             .update(str)
             .digest('hex');
+    }
+
+    static MD5_STRETCH(str) {
+        let hash = this.MD5(str);
+
+        for (let i = 0; i < 2016; i++) {
+            hash = this.MD5(hash);
+        }
+
+        return hash;
     }
 
     md5(str) {
@@ -42,13 +38,37 @@ class KeyGenerator {
         return this.hashCache[str];
     }
 
+    md5Stretch(str) {
+        if (!this.hashCache[str]) {
+            this.hashCache[str] = KeyGenerator.MD5_STRETCH(str);
+        }
+
+        return this.hashCache[str];
+    }
+
+    testHashForValidity(match) {
+        // Take our 3 char match and convert it to a 5 char match
+        match = match + match[0] + match[0];
+
+        for (let i = 0; i < 1000; i++) {
+            let input = `${this.salt}${this.index + i}`;
+            let hash = this.use_stretch_hash ? this.md5Stretch(input) : this.md5(input);
+
+            if (hash.includes(match)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     generateNextKeys(n = 64) {
         for (let key = 0; key < n; key++) {
             let found_key = false;
 
             while (found_key === false) {
                 let input = `${this.salt}${this.index++}`;
-                let hash = this.md5(input);
+                let hash = this.use_stretch_hash ? this.md5Stretch(input) : this.md5(input);
 
                 if (THREE_CHARS_REGEX.test(hash)) {
                     // Hash has three repeating characters in a row!
