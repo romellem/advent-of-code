72c3df4ff20358e0ef9dfcf28e6dfc60ee177282
romellem <romellem.git@gmail.com>
2019-03-31 15:41:44 -0500

    After looking at the data, create algo for solution

--- 2016/19/part-two.js
+++ 2016/19/part-two.js
@@ -1,44 +1,23 @@
-const assert = require('assert');
 const NUMBER_OF_ELVES = require('./input');
-const { LoopedList } = require('looped-list');
 
-const loopElves = arr => {
-    let length = arr.length;
-    let list = new LoopedList(arr);
+const getLastElf = n => {
+    // Get largest exponent part of input (`floor(log_3(n))`)
+    let exp = Math.floor(Math.log(n) / Math.log(3));
+    let largest_pow_of_3 = Math.pow(3, exp);
 
-    /**
-     * I realized, I don't need to check if the next elf is the same
-     * as the current elf, just loop a preset number of times.
-     */
-    while ((length--) > 1) {
-        process.stdout.write(length + '\r');
-        let current_elf = list.head;
-        /**
-         * Elf "across the circle" is the length divided by two,
-         * with odd numbers having the elf "to the left" be selected,
-         * which means we round down.
-         */
-        let elf_across_circle_movements = Math.floor((length + 1) / 2);
-        let elf_across_circle = list.move(elf_across_circle_movements).head;
-
-        // Elf across the circle is removed
-        elf_across_circle.removeSelf();
-
-        // Set the list's "head" to the elf to the left of the current elf
-        list.head = current_elf.next_item;
+    // If our input is a pure power of 3, the "winner" is that input
+    if (n === largest_pow_of_3) {
+        return n;
     }
 
-    return list.head.value;
+    // If our input is _less than or equal to_ two of our highest exponent,
+    // the answer is our input minus the highest power
+    if (n <= 2 * largest_pow_of_3) {
+        return n - largest_pow_of_3;
+    }
+
+    // Otherwise, it is double our input, minus triple our largest power
+    return (2 * n) - (3 * largest_pow_of_3);
 };
 
-// Tests
-assert.strictEqual(loopElves([1, 2, 3, 4, 5]), 2);
-console.log('Tests passed');
-
-const elves = Array(NUMBER_OF_ELVES)
-    .fill()
-    .map((c, i) => i + 1);
-
-const answer = loopElves(elves);
-console.log('                 ');
-console.log(answer);
+console.log(getLastElf(NUMBER_OF_ELVES));
