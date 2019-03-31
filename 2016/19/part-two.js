c7daf8690196b71f565ef8323634558a40ce913f
romellem <romellem.git@gmail.com>
2019-03-31 15:16:37 -0500

    Revert "This didn't work!"
    This reverts commit 71b009b6ebca95eb502f7f70dc504cfcdbd3ec00.

--- 2016/19/part-two.js
+++ 2016/19/part-two.js
@@ -1,35 +1,34 @@
 const assert = require('assert');
 const NUMBER_OF_ELVES = require('./input');
+const { LoopedList } = require('looped-list');
 
 const loopElves = arr => {
-    let list = arr.slice(0);
-
-    let current = 0;
+    let length = arr.length;
+    let list = new LoopedList(arr);
 
     /**
      * I realized, I don't need to check if the next elf is the same
      * as the current elf, just loop a preset number of times.
      */
-    while (list.length > 1) {
-        let current_item = list[current];
-
-        if (list.length % 241 === 0) process.stdout.write(list.length + '\r');
+    while ((length--) > 1) {
+        process.stdout.write(length + '\r');
+        let current_elf = list.head;
         /**
          * Elf "across the circle" is the length divided by two,
          * with odd numbers having the elf "to the left" be selected,
          * which means we round down.
          */
-        let elf_across_circle_movements = Math.floor(list.length / 2);
-        let elf_across_circle_index = (current + elf_across_circle_movements) % list.length;
-        list.splice(elf_across_circle_index, 1);
+        let elf_across_circle_movements = Math.floor((length + 1) / 2);
+        let elf_across_circle = list.move(elf_across_circle_movements).head;
 
-        if (current > list.length - 1) {
-            current = list.indexOf(current_item);
-        }
-        current = (current + 1);
+        // Elf across the circle is removed
+        elf_across_circle.removeSelf();
+
+        // Set the list's "head" to the elf to the left of the current elf
+        list.head = current_elf.next_item;
     }
 
-    return list[0];
+    return list.head.value;
 };
 
 // Tests
