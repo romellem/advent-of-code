fe0b7afe8a4f3f13c4922dd2b59ccae1b94a995a
romellem <romellem.git@gmail.com>
2019-06-01 19:52:19 -0500

    Init firewall class, doesn't work
    The scanners don't loop, they bounce back and forth. Whoops!

--- 2017/13/firewall.js
+++ 2017/13/firewall.js
@@ -1,5 +1,72 @@
-class Firewall {
+const { mapValues, keyBy } = require('lodash');
 
+class Layer {
+    constructor(range = 0) {
+        this.range = range;
+
+        this.scanner = null;
+        if (this.range > 0) {
+            // Initialize the scanner at position 0
+            this.scanner = 0;
+        }
+    }
+
+    tick() {
+        if (this.range > 0) {
+            this.scanner++;
+            this.scanner %= this.range;
+        }
+
+        return this.scanner;
+    }
+}
+
+class Firewall {
+    constructor(layers) {
+        // Poor man's deep clone
+        // this.layers = JSON.parse(JSON.stringify(layers));
+        let max_depth = this.getMaxDepth(layers);
+        let layers_lookup = this.convertLayersArrayToMap(layers);
+
+        // `max_depth + 1` because the layers are zero-indexed
+        this.layers = Array(max_depth + 1)
+            .fill()
+            .map((layer, depth) => new Layer(layers_lookup[depth]));
+
+        this.times_caught = [];
+    }
+
+    getMaxDepth(layers) {
+        return Math.max.apply(null, layers.map(l => l.depth));
+    }
+
+    /**
+     * @param {Array} layers - Array of { depth, range } objects
+     * @returns {Object} - Returns an object, keyed by `depth`, with its value as the `range`
+     */
+    convertLayersArrayToMap(layers) {
+        return mapValues(keyBy(layers, 'depth'), 'range');
+    }
+
+    moveThrough(checkPosition = 0) {
+        this.layers.forEach((layer, time) => {
+            /**
+             * First, particle enters (determined by `time`),
+             * so we see if the layer at `time` has the scanner
+             * in position `0`. If so, push a "time_caught"
+             * to our tracker array, with `time` multiplied
+             * by the layers `range`.
+             */
+            if (layer.scanner === checkPosition) {
+                this.times_caught.push(time * layer.range);
+            }
+
+            // Next, tick all layers' scanners forward
+            this.layers.forEach(l => l.tick());
+        });
+
+        return this.times_caught.reduce((a, b) => a + b, 0);
+    }
 }
 
 module.exports = Firewall;
--- 2017/13/part-one.js
+++ 2017/13/part-one.js
@@ -1,2 +1,8 @@
 const assert = require('assert');
 const { input, sampleInput } = require('./input');
+const Firewall = require('./firewall');
+
+// Tests
+let test_firewall = new Firewall(sampleInput.config);
+let test_severity = test_firewall.moveThrough();
+assert.strictEqual(test_severity, sampleInput.severity);
