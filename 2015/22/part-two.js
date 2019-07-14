bf47302024a0426ddb43902dd9c19f6cf3d1bc7f
romellem <romellem.git@gmail.com>
2019-07-14 09:34:13 -0500

    Pulls in changes from part one to part two

--- 2015/22/part-two.js
+++ 2015/22/part-two.js
@@ -1,13 +1,16 @@
-var state = {
+let initial_state = {
     boss: 51,
-    d: 9,
+    damage: 9,
+
     hp: 50,
-    m: 500,
-    a: 0,
+    mana: 500,
+    armor: 0,
+    manaSpent: 0,
+
     shield: 0,
     poison: 0,
     recharge: 0,
-    manaspent: 0,
+
     move: 'player',
 };
 
@@ -16,57 +19,91 @@ const DRAIN = 'drain';
 const SHIELD = 'shield';
 const POISON = 'poison';
 const RECHARGE = 'recharge';
-var spells = {
-    [MISSILE]: [53, 4, 0],
-    [DRAIN]: [73, 2, 2],
-    [SHIELD]: [113, 6],
-    [POISON]: [173, 6],
-    [RECHARGE]: [229, 5],
+const spells = {
+    [MISSILE]: { cost: 53, damage: 4, heal: 0 },
+    [DRAIN]: { cost: 73, damage: 2, heal: 2 },
+    [SHIELD]: { cost: 113, duration: 6 },
+    [POISON]: { cost: 173, duration: 6 },
+    [RECHARGE]: { cost: 229, duration: 5 },
 };
 
-var min = Number.MAX_SAFE_INTEGER;
-var moves = [state];
-var state;
+let min = Number.MAX_SAFE_INTEGER;
+let moves = [initial_state];
+let state;
 
-while ((state = moves.shift())) {
-    // "Hard mode"
-    if (state['move'] === 'player') state['hp']--;
+while ((state = moves.pop())) {
+    // On hard mode, player loses 1 HP each turn
+    if (state.move === 'player') {
+        state.hp--;
+    }
 
-    state['a'] = state['shield']-- > 0 ? 7 : 0;
-    if (state['poison']-- > 0) state['boss'] -= 3;
-    if (state['recharge']-- > 0) state['m'] += 101;
+    // Check effects and apply them if they are greater than 0
+    // This ends up reducing these effects below zero in some cases but it doesn't end up affecting anything
+    state.armor = state.shield-- > 0 ? 7 : 0;
 
-    if (state['hp'] <= 0 || state['manaspent'] >= min) continue;
-    if (state['boss'] <= 0) {
-        min = Math.min(min, state['manaspent']);
+    if (state.poison-- > 0) {
+        state.boss -= 3;
+    }
+
+    if (state.recharge-- > 0) {
+        state.mana += 101;
+    }
+
+    // If we've lost all our HP, or we've spent more mana than our current minimum we've found, exit this path
+    if (state.hp <= 0 || state.manaSpent >= min) {
         continue;
     }
 
-    if (state['move'] === 'boss') {
-        state['move'] = 'player';
-        state['hp'] -= Math.max(1, state['d'] - state['a']);
-        moves.unshift(state);
+    // If our boss is dead, see if our mana spent is less than our current minimum
+    if (state.boss <= 0) {
+        min = Math.min(min, state.manaSpent);
+        continue;
+    }
+
+    if (state.move === 'boss') {
+        // Switch to player for next move
+        state.move = 'player';
+
+        // Boss always does at least 1 damage
+        state.hp -= Math.max(1, state.damage - state.armor);
+
+        moves.push(state);
+
         // console.log('  Boss { ' + Object.keys(state).map(k => `${k === 'move' ? k + ': "' + state[k] + '"' : k + ': ' + state[k]}`).join(', ') + ' }');
     } else {
-        state['move'] = 'boss';
-        for (let spell in spells) {
-            let info = spells[spell];
-            if (info[0] >= state['m']) continue;
-            let n = JSON.parse(JSON.stringify(state));
-            n['m'] -= info[0];
-            n['manaspent'] += info[0];
+        // Switch to boss for next move
+        state.move = 'boss';
 
-            if (spell === MISSILE || spell === DRAIN) {
-                n['boss'] -= info[1];
-                n['hp'] += info[2];
-            } else {
-                if (n[spell] > 0) {
-                    continue;
-                }
-                n[spell] = info[1];
+        // We are on the player, loop through all the spells and push them to our possible moves
+        for (let spell in spells) {
+            let { cost, damage, heal, duration } = spells[spell];
+
+            // If this costs more than the mana we have, skip it
+            if (cost >= state.mana) {
+                continue;
             }
 
-            moves.unshift(n);
+            // Clone our current state
+            let new_state = Object.assign({}, state);
+
+            new_state.mana -= cost;
+            new_state.manaSpent += cost;
+
+            if (spell === MISSILE || spell === DRAIN) {
+                // Missle and Drain have no effect, they are instant spells
+                new_state.boss -= damage;
+                new_state.hp += heal;
+            } else {
+                // If we've already activated this effect, skip it
+                if (new_state[spell] > 0) {
+                    continue;
+                }
+
+                new_state[spell] = duration;
+            }
+
+            moves.push(new_state);
+
             // console.log('Player { ' + Object.keys(state).map(k => `${k === 'move' ? k + ': "' + n[k] + '"' : k + ': ' + n[k]}`).join(', ') + ' }');
         }
     }
