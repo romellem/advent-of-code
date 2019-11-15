e893cc3a1adb292ed614d7470e047a441d1b8e13
romellem <romellem.git@gmail.com>
2019-11-15 10:26:08 -0600

    Working part one

--- 2017/24/part-one.js
+++ 2017/24/part-one.js
@@ -2,11 +2,16 @@ const assert = require('assert');
 const { input, sampleInput } = require('./input');
 const Bridges = require('./bridge');
 
-let bridges = new Bridges(sampleInput);
+let test_bridges = new Bridges(sampleInput);
+let test_best_bridge = test_bridges.getBestSolution();
+
+assert.strictEqual(Bridges.getSolutionScore(test_best_bridge), 31);
+
+let bridges = new Bridges(input);
 let best_bridge = bridges.getBestSolution();
 
-console.log('All Bridges\n============');
-bridges.printSolutions();
+// console.log('All Bridges\n============');
+// bridges.printSolutions();
 console.log('============\nSolution:\n');
 console.log(Bridges.getSolutionString(best_bridge));
 console.log(Bridges.getSolutionScore(best_bridge));
