01d528b4552346f5ee0fb1557b6f85509c955732
romellem <romellem.git@gmail.com>
2019-11-15 20:37:10 -0600

    Working part two!

--- 2017/18/part-two.js
+++ 2017/18/part-two.js
@@ -1,6 +1,10 @@
-const { input, sampleInput } = require('./input');
+const { input, sampleParallelInput } = require('./input');
 const assert = require('assert');
 const ParallelPrograms = require('./parallel');
+
+let test_program = new ParallelPrograms(sampleParallelInput);
+let test_times_sent = test_program.orchestrateRun();
+assert.strictEqual(test_times_sent, 3);
 
 let program = new ParallelPrograms(input);
 let times_sent = program.orchestrateRun();
