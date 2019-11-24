1a1a9d1a7a8d2dfcb4243c67ac5a8bb29ff14f35
romellem <romellem.git@gmail.com>
2019-11-24 10:20:05 -0600

    Part one tests

--- 2016/23/part-one.js
+++ 2016/23/part-one.js
@@ -3,8 +3,8 @@ const { sampleInput, input } = require('./input');
 const Device = require('./device');
 
 // Tests
-// let test_device = new Device(sampleInput.instructions);
-// assert.strictEqual(test_device.run(), sampleInput.registerAResult);
+let test_device = new Device(sampleInput.instructions);
+assert.strictEqual(test_device.run(), sampleInput.registerAResult);
 
 let device = new Device(input);
 let register_a = device.run();
