5c4f1dddbdfabb1fec770fdc40931c5b38cd61fe
romellem <romellem.git@gmail.com>
2019-11-30 23:20:48 -0600

    Working part one and two

--- 2019/1/part-two.js
+++ 2019/1/part-two.js
@@ -1,1 +1,21 @@
 const { input } = require('./input');
+
+// To find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+// Continue recursively for the mass of the _fuel_ you just calculated. If the mass is zero or less, discard it
+const calcFuelArray = (mass, arr = []) => {
+	let fuel = Math.floor(mass / 3) - 2;
+	if (fuel <= 0) {
+		return arr;
+	} else {
+		arr.push(fuel);
+		return calcFuelArray(fuel, arr);
+	}
+};
+
+calcTotalFuel = mass => {
+	return calcFuelArray(mass).reduce((a, b) => a + b, 0);
+};
+
+let fuel = input.map(mass => calcTotalFuel(mass));
+const total_fuel = fuel.reduce((a, b) => a + b, 0);
+console.log(total_fuel);
