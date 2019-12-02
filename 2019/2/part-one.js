6cef5e09f281052a91ead6108751afd959b146b3
romellem <romellem.git@gmail.com>
2019-12-01 23:51:49 -0600

    Refactors code to class based

--- 2019/2/part-one.js
+++ 2019/2/part-one.js
@@ -1,25 +1,6 @@
 const { input } = require("./input");
+const Computer = require("./computer");
 
-const ADD = 1;
-const MULTP = 2;
-const STOP = 99;
+const computer = new Computer(input);
 
-input[1] = 12;
-input[2] = 2;
-
-for (let i = 0; i < input.length; i += 4) {
-	let op = input[i];
-	let num1 = input[i + 1];
-	let num2 = input[i + 2];
-	let dest = input[i + 3];
-
-	if (op === ADD) {
-		input[dest] = input[num1] + input[num2];
-	} else if (op === MULTP) {
-		input[dest] = input[num1] * input[num2];
-	} else if (op === STOP) {
-		break;
-	}
-}
-
-console.log(input[0]);
+console.log(computer.partOne());
--- 2019/2/part-two.js
+++ 2019/2/part-two.js
@@ -1,44 +1,6 @@
 const { input } = require("./input");
+const Computer = require("./computer");
 
-const ADD = 1;
-const MULTP = 2;
-const STOP = 99;
+const computer = new Computer(input);
 
-const findInput = () => {
-	let nums = Array(100)
-		.fill(0)
-		.map((c, i) => i);
-
-	while (true) {
-		for (let noun of nums) {
-			for (let verb of nums) {
-				const list = input.slice(0);
-				list[1] = noun;
-				list[2] = verb;
-
-				for (let i = 0; i < list.length; i += 4) {
-					let op = list[i];
-					let num1 = list[i + 1];
-					let num2 = list[i + 2];
-					let dest = list[i + 3];
-
-					if (op === ADD) {
-						list[dest] = list[num1] + list[num2];
-					} else if (op === MULTP) {
-						list[dest] = list[num1] * list[num2];
-					} else if (op === STOP) {
-						break;
-					}
-				}
-
-				if (list[0] === 19690720) {
-					return 100 * noun + verb;
-				}
-			}
-		}
-	}
-};
-
-let ans = findInput();
-
-console.log(ans);
+console.log(computer.partTwo());
