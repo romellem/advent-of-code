3da7e8034ea7cd43fe8c6ab9cd1b906331ce1efc
romellem <romellem.git@gmail.com>
2019-12-01 20:54:40 -0600

    Uses parsing method for input

--- 2016/11/input.js
+++ 2016/11/input.js
@@ -1,82 +1,25 @@
-const { GENERATOR, MICROCHIP, Floor } = require('./rtg-path');
+const { RTGPath } = require('./rtg-path');
+const parse = RTGPath.parseInputStringToStartingArrangementObject;
 
-/*
-The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+const TEST_INPUT = `The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
 The second floor contains a hydrogen generator.
 The third floor contains a lithium generator.
-The fourth floor contains nothing relevant.
-*/
-const sampleInput = {
-	elevator: 0,
-	floors: [
-		new Floor([
-			{ type: MICROCHIP, element: 'H' },
-			{ type: MICROCHIP, element: 'L' },
-		]),
-		new Floor([{ type: GENERATOR, element: 'H' }]),
-		new Floor([{ type: GENERATOR, element: 'L' }]),
-		new Floor(),
-	],
-};
+The fourth floor contains nothing relevant.`;
 
-// I use 'silver' instead of 'promethium' and
-// 'hydrogen' instead of 'cobalt', just to make things easy with the initials
-/*
-The first floor contains a silver generator and a silver-compatible microchip.
-The second floor contains a hydrogen generator, a curium generator, a ruthenium generator, and a plutonium generator.
-The third floor contains a hydrogen-compatible microchip, a curium-compatible microchip, a ruthenium-compatible microchip, and a plutonium-compatible microchip.
-The fourth floor contains nothing relevant.
-*/
-const input = {
-	elevator: 0,
-	floors: [
-		new Floor([
-			{ type: GENERATOR, element: 'S' },
-			{ type: MICROCHIP, element: 'S' },
-		]),
-		new Floor([
-			{ type: GENERATOR, element: 'H' },
-			{ type: GENERATOR, element: 'C' },
-			{ type: GENERATOR, element: 'R' },
-			{ type: GENERATOR, element: 'P' },
-		]),
-		new Floor([
-			{ type: MICROCHIP, element: 'H' },
-			{ type: MICROCHIP, element: 'C' },
-			{ type: MICROCHIP, element: 'R' },
-			{ type: MICROCHIP, element: 'P' },
-		]),
-		new Floor(),
-	],
-};
+const INPUT = `The first floor contains a promethium generator and a promethium-compatible microchip.
+The second floor contains a cobalt generator, a curium generator, a ruthenium generator, and a plutonium generator.
+The third floor contains a cobalt-compatible microchip, a curium-compatible microchip, a ruthenium-compatible microchip, and a plutonium-compatible microchip.
+The fourth floor contains nothing relevant.`;
 
 // Plus: elerium and dilithium chip/gen pair on first floor
-const partTwoInput = {
-	elevator: 0,
-	floors: [
-		new Floor([
-			{ type: GENERATOR, element: 'S' },
-			{ type: MICROCHIP, element: 'S' },
-			{ type: GENERATOR, element: 'E' },
-			{ type: MICROCHIP, element: 'E' },
-			{ type: GENERATOR, element: 'D' },
-			{ type: MICROCHIP, element: 'D' },
-		]),
-		new Floor([
-			{ type: GENERATOR, element: 'H' },
-			{ type: GENERATOR, element: 'C' },
-			{ type: GENERATOR, element: 'R' },
-			{ type: GENERATOR, element: 'P' },
-		]),
-		new Floor([
-			{ type: MICROCHIP, element: 'H' },
-			{ type: MICROCHIP, element: 'C' },
-			{ type: MICROCHIP, element: 'R' },
-			{ type: MICROCHIP, element: 'P' },
-		]),
-		new Floor(),
-	],
-};
+const PART_TWO_INPUT = `The first floor contains a promethium generator, an elerium generator, an elerium-compatible microchip, a dilithium generator, a dilithium-compatible microchip, and a promethium-compatible microchip.
+The second floor contains a cobalt generator, a curium generator, a ruthenium generator, and a plutonium generator.
+The third floor contains a cobalt-compatible microchip, a curium-compatible microchip, a ruthenium-compatible microchip, and a plutonium-compatible microchip.
+The fourth floor contains nothing relevant.`;
+
+const sampleInput = parse(TEST_INPUT);
+const input = parse(INPUT);
+const partTwoInput = parse(PART_TWO_INPUT);
 
 module.exports = {
 	sampleInput,
