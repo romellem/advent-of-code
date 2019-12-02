1441d644ff2a36a82f67846c4c74367750333320
Matt Wade <romellem@users.noreply.github.com>
2019-12-02 15:29:35 -0600

    Fixes syntax error

--- docs/assets/js/2016/25/main.bundle.js
+++ docs/assets/js/2016/25/main.bundle.js
@@ -70,7 +70,7 @@ const Registers = ({
 			React.createElement('li', null, 'c: ', c),
 			React.createElement('li', null, 'd: ', d)
 		),
-	  React.createElement("br", null)
+	  React.createElement("br", null),
 	    'Signal: ',
 	    signal
 	);
