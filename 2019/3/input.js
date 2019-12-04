ef8cabaca2cb1d8e86b060653fed4a330cf634c9
romellem <romellem.git@gmail.com>
2019-12-03 18:18:06 -0600

    Adds tests, fails
    I think I see why:
    
    > While the wires do technically cross right at the central port where they both start,
    > this point does not count, nor does a wire count as crossing with itself.
    
    I think I am counting when a wire crosses with itself.

--- 2019/3/input.js
+++ 2019/3/input.js
@@ -1,4 +1,27 @@
+parseWire = wire => {
+	return wire.split(',').map(movement => {
+		return [movement.substring(0, 1), parseInt(movement.substring(1))];
+	});
+}
+
 module.exports = {
+	sampleInputs: [
+		{
+			wire_a: parseWire('R8,U5,L5,D3'),
+			wire_b: parseWire('U7,R6,D4,L4'),
+			closest: 6,
+		},
+		{
+			wire_a: parseWire('R75,D30,R83,U83,L12,D49,R71,U7,L72'),
+			wire_b: parseWire('U62,R66,U55,R34,D71,R55,D58,R83'),
+			closest: 159,
+		},
+		{
+			wire_a: parseWire('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'),
+			wire_b: parseWire('U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'),
+			closest: 135,
+		},
+	],
 	input: {
 		wire_a: [
 			['R', 990],
--- 2019/3/part-one.js
+++ 2019/3/part-one.js
@@ -1,5 +1,6 @@
-const { input } = require('./input');
+const { input, sampleInputs } = require('./input');
 const manhattan = require('manhattan');
+const assert = require('assert');
 
 let { wire_a, wire_b } = input;
 
@@ -23,61 +24,78 @@ class Grid {
 			return false;
 		}
 	}
-}
 
-let grid = new Grid();
-let crosses = {};
-
-[wire_a, wire_b].forEach(wire => {
-	let x = 0;
-	let y = 0;
-
-	// Could DRY this section up a bit... but this seems fairly clear
-	wire.forEach(movement => {
-		let [dir, distance] = movement;
-		if (dir === 'R') {
-			for (let i = x + 1; i <= x + distance; i++) {
-				if (grid.increment(i, y)) {
-					crosses[i + ',' + y] = true;
-				}
-			}
-			x += distance;
-		} else if (dir === 'L') {
-			for (let i = x - 1; i >= x - distance; i--) {
-				if (grid.increment(i, y)) {
-					crosses[i + ',' + y] = true;
-				}
-			}
-			x -= distance;
-		} else if (dir === 'U') {
-			for (let i = y + 1; i <= y + distance; i++) {
-				if (grid.increment(x, i)) {
-					crosses[x + ',' + i] = true;
-				}
-			}
-			y += distance;
-		} else if (dir === 'D') {
-			for (let i = y - 1; i >= y - distance; i--) {
-				if (grid.increment(x, i)) {
-					crosses[x + ',' + i] = true;
-				}
-			}
-			y -= distance;
-		} else {
-			throw move_str;
+	// While the wires do technically cross right at the central port where they both start, this point does not count, nor does a wire count as crossing with itself.
+	set(x, y) {
+		const cell = `${x},${y}`;
+		if (this.grid[cell] == null) {
+			this.grid[cell] = 1;
 		}
-	});
-});
-
-let closest_distance = Number.MAX_SAFE_INTEGER;
-for (let cross of Object.keys(crosses)) {
-	let [x, y] = cross.split(',').map(v => parseInt(v, 10));
-	console.log(x, y);
-	let distance = manhattan([x, y], [0, 0]);
-
-	if (distance < closest_distance) {
-		closest_distance = distance;
 	}
 }
 
-console.log(closest_distance);
+const calculateShortestCrossoverFromOrigin = (wire_a, wire_b) => {
+	let grid = new Grid();
+	let crosses = {};
+
+	[wire_a, wire_b].forEach((wire, wire_num) => {
+		let x = 0;
+		let y = 0;
+
+		// Could DRY this section up a bit... but this seems fairly clear
+		wire.forEach(movement => {
+			let [dir, distance] = movement;
+			if (dir === 'R') {
+				for (let i = x + 1; i <= x + distance; i++) {
+					if (grid.increment(i, y)) {
+						crosses[i + ',' + y] = true;
+					}
+				}
+				x += distance;
+			} else if (dir === 'L') {
+				for (let i = x - 1; i >= x - distance; i--) {
+					if (grid.increment(i, y)) {
+						crosses[i + ',' + y] = true;
+					}
+				}
+				x -= distance;
+			} else if (dir === 'U') {
+				for (let i = y + 1; i <= y + distance; i++) {
+					if (grid.increment(x, i)) {
+						crosses[x + ',' + i] = true;
+					}
+				}
+				y += distance;
+			} else if (dir === 'D') {
+				for (let i = y - 1; i >= y - distance; i--) {
+					if (grid.increment(x, i)) {
+						crosses[x + ',' + i] = true;
+					}
+				}
+				y -= distance;
+			} else {
+				throw move_str;
+			}
+		});
+	});
+
+	let closest_distance = Number.MAX_SAFE_INTEGER;
+	for (let cross of Object.keys(crosses)) {
+		let [x, y] = cross.split(',').map(v => parseInt(v, 10));
+		console.log(x, y);
+		let distance = manhattan([x, y], [0, 0]);
+
+		if (distance < closest_distance) {
+			closest_distance = distance;
+		}
+	}
+
+	return closest_distance;
+};
+
+for (let { wire_a, wire_b, closest } of sampleInputs) {
+	assert.strictEqual(calculateShortestCrossoverFromOrigin(wire_a, wire_b), closest);
+}
+
+console.log(calculateShortestCrossoverFromOrigin(wire_a, wire_b));
+
