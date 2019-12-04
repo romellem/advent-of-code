65dd81e2547587fda54f64a5b9a5939aeb9f838b
romellem <romellem.git@gmail.com>
2019-12-03 18:04:51 -0600

    Uses an object for an infinite grid, only set cells I cross
    This seems to work for the input but I don't get the correct answer...

--- 2019/3/part-one.js
+++ 2019/3/part-one.js
@@ -1,99 +1,68 @@
 const { input } = require('./input');
 const manhattan = require('manhattan');
 
-const parse = wire => {
-	let lets = wire.split('');
-	let dir = lets.shift();
-	return { dir, distance: parseInt(lets.join('')) };
-};
+let { wire_a, wire_b } = input;
 
-let [wire_a, wire_b] = input;
-
-// Highest y and x for each wire
-let max_x = 0;
-let max_y = 0;
-
-let min_x = 0;
-let min_y = 0;
-
-let x = 0,
-	y = 0;
-wire_a.forEach(move_str => {
-	let { dir, distance } = parse(move_str);
-	if (dir === 'R') {
-		x += distance;
-		max_x = Math.max(x, max_x);
-	} else if (dir === 'L') {
-		x -= distance;
-		min_x = Math.min(x, min_x);
-	} else if (dir === 'U') {
-		y += distance;
-		max_y = Math.max(y, max_y);
-	} else if (dir === 'D') {
-		x -= distance;
-		min_y = Math.min(y, min_y);
-	} else {
-		throw move_str;
+class Grid {
+	constructor() {
+		this.grid = { '0,0': 0 };
 	}
-});
 
-// console.log(max_x,
-//     max_y,
-//     min_x,
-//     min_y);
+	increment(x, y) {
+		const cell = `${x},${y}`;
+		if (this.grid[cell] == null) {
+			this.grid[cell] = 0;
+		}
 
-(x = 0), (y = 0);
-wire_b.forEach(move_str => {
-	let { dir, distance } = parse(move_str);
-	if (dir === 'R') {
-		x += distance;
-		max_x = Math.max(x, max_x);
-	} else if (dir === 'L') {
-		x -= distance;
-		min_x = Math.min(x, min_x);
-	} else if (dir === 'U') {
-		y += distance;
-		max_y = Math.max(y, max_y);
-	} else if (dir === 'D') {
-		x -= distance;
-		min_y = Math.min(y, min_y);
-	} else {
-		throw move_str;
+		this.grid[cell]++;
+
+		// Return true if more than 1 (crossover), false otherwise
+		if (this.grid[cell] > 1) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-});
+}
 
-let x_size = Math.abs(max_x - min_x);
-let y_size = Math.abs(max_y - min_y);
-
-let grid = Array(y_size)
-	.fill()
-	.map(row => Array(x_size).fill(0));
+let grid = new Grid();
+let crosses = {};
 
 [wire_a, wire_b].forEach(wire => {
-	let wire_a_x = 0;
-	let wire_a_y = 0;
-	wire.forEach(move_str => {
-		let { dir, distance } = parse(move_str);
+	let x = 0;
+	let y = 0;
+
+	// Could DRY this section up a bit... but this seems fairly clear
+	wire.forEach(movement => {
+		let [dir, distance] = movement;
 		if (dir === 'R') {
-			for (let i = 1; i <= distance; i++) {
-				grid[wire_a_y][wire_a_x + i]++;
+			for (let i = x + 1; i <= x + distance; i++) {
+				if (grid.increment(i, y)) {
+					crosses[i + ',' + y] = true;
+				}
 			}
-			wire_a_x += distance;
+			x += distance;
 		} else if (dir === 'L') {
-			for (let i = distance; i >= 1; i--) {
-				grid[wire_a_y][wire_a_x + i]++;
+			for (let i = x - 1; i >= x - distance; i--) {
+				if (grid.increment(i, y)) {
+					crosses[i + ',' + y] = true;
+				}
 			}
-			wire_a_x -= distance;
+			x -= distance;
 		} else if (dir === 'U') {
-			for (let i = 1; i <= distance; i++) {
-				grid[wire_a_y + i][wire_a_x]++;
+			for (let i = y + 1; i <= y + distance; i++) {
+				if (grid.increment(x, i)) {
+					crosses[x + ',' + i] = true;
+				}
 			}
-			wire_a_y += distance;
+			y += distance;
 		} else if (dir === 'D') {
-			for (let i = distance; i >= 1; i--) {
-				grid[wire_a_y + i][wire_a_x]++;
+			for (let i = y - 1; i >= y - distance; i--) {
+				if (grid.increment(x, i)) {
+					crosses[x + ',' + i] = true;
+				}
 			}
-			wire_a_y -= distance;
+			y -= distance;
 		} else {
 			throw move_str;
 		}
@@ -101,16 +70,14 @@ let grid = Array(y_size)
 });
 
 let closest_distance = Number.MAX_SAFE_INTEGER;
-for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[0].length; x++) {
-        let cell = grid[y][x];
-        if (cell > 1) {
-            let d = manhattan([x, y], [0, 0]);
-            if (d < closest_distance) {
-                d = closest_distance;
-            }
-        }
-    }
+for (let cross of Object.keys(crosses)) {
+	let [x, y] = cross.split(',').map(v => parseInt(v, 10));
+	console.log(x, y);
+	let distance = manhattan([x, y], [0, 0]);
+
+	if (distance < closest_distance) {
+		closest_distance = distance;
+	}
 }
 
-console.log(d)
+console.log(closest_distance);
