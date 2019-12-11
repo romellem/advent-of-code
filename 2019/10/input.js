d7077c45c9f2b79d92f5ea8e4622cffd675b97cb
romellem <romellem.git@gmail.com>
2019-12-10 20:43:56 -0600

    Fixes sample inputs

--- 2019/10/input.js
+++ 2019/10/input.js
@@ -2,7 +2,7 @@ const inputToArray = inputStr =>
 	String(inputStr)
 		.trim()
 		.split('\n')
-		.map(row => row.split(''));
+		.map(row => row.split('').map(v => (v === '#' ? 1 : 0)));
 
 const main_input = `#.....#...#.........###.#........#..
 ....#......###..#.#.###....#......##
@@ -67,7 +67,7 @@ const sample_input_3 = `#.#...#.#.
 ..#...##..
 ..##....##
 ......#...
-.####.###.`
+.####.###.`;
 
 const sample_input_4 = `.#..#..###
 ####.###.#
@@ -101,7 +101,6 @@ const sample_input_5 = `.#..##.###...#######
 #.#.#.#####.####.###
 ###.##.####.##.#..##`;
 
-
 module.exports = {
 	parseInt: inputToArray,
 	input: inputToArray(main_input),
@@ -109,27 +108,27 @@ module.exports = {
 		{
 			grid: inputToArray(sample_input_1),
 			best_count: 8,
-			best_coords = [3, 4],
+			best_coords: [3, 4],
 		},
 		{
 			grid: inputToArray(sample_input_2),
 			best_count: 33,
-			best_coords = [5, 8],
+			best_coords: [5, 8],
 		},
 		{
 			grid: inputToArray(sample_input_3),
 			best_count: 35,
-			best_coords = [1, 2],
+			best_coords: [1, 2],
 		},
 		{
 			grid: inputToArray(sample_input_4),
 			best_count: 41,
-			best_coords = [6, 3],
+			best_coords: [6, 3],
 		},
 		{
 			grid: inputToArray(sample_input_5),
 			best_count: 210,
-			best_coords = [11, 13],
+			best_coords: [11, 13],
 		},
 	],
 };
