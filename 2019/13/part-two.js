2078e5fd3330beb592f0e5cd6bb2e09223d22b76
romellem <romellem.git@gmail.com>
2019-12-13 10:07:54 -0600

    Test grid output
    Going to have to debug this even more, I'm hoping that the ball will be
    _at most_ one unit away from the paddle, and so we only have to decide
    to move the paddle right when it asks for an input.
    
    This is fairly clever, but tough! I feel like my setup for how `inputs`
    and `outputs` work in my intcode computer isn't ideal for these
    scenarios, but I'm not sure. I may end up refactoring that part.

--- 2019/13/part-two.js
+++ 2019/13/part-two.js
@@ -1,2 +1,14 @@
 const { input } = require('./input');
 const { Arcade } = require('./intcode-computer');
+
+let arcade = new Arcade(input);
+let grid = arcade.runAndGetGrid();
+grid.printGrid()
+
+// Memory address 0 represents the number of quarters that have been inserted;
+// set it to 2 to play for free.
+// input[0] = 2;
+// let arcade = new Arcade(input);
+// let output = arcade.run();
+
+// console.log(output);
