68312490809c2eca81efe6a756d89667778aae0d
romellem <romellem.git@gmail.com>
2019-12-13 13:45:36 -0600

    Mostly empty commit, I think I got it
    Just like a 'pong' AI, just have the paddle _always_ follow the ball!
    So whenever I get to an input instruction, look at the ball's x,y coords
    and the paddle's x,y coords. Really, just its x position. If the ball is
    less, move left; if its greater, move right; otherwise do nothing.
    
    The manner in which I am _sending_ the inputs is (still) a little strange. It
    feels clunky. Not quite sure how to make this more robust though...

--- 2019/13/part-two.js
+++ 2019/13/part-two.js
@@ -3,7 +3,8 @@ const { Arcade } = require('./intcode-computer');
 
 let arcade = new Arcade(input);
 let grid = arcade.runAndGetGrid();
-grid.printGrid()
+grid.printGrid();
+console.log(grid.boundaries);
 
 // Memory address 0 represents the number of quarters that have been inserted;
 // set it to 2 to play for free.
