e7f0e0f9b87051e3297b26759f9e989ada6ea194
romellem <romellem.git@gmail.com>
2019-12-15 13:25:42 -0600

    More refactoring, still don't have it
    This is one I just can't wrap my head around the math.

--- 2019/14/formula.js
+++ 2019/14/formula.js
@@ -74,28 +74,25 @@ class Formula {
 			return amount;
 		}
 
-		// Get formula pieces (the "needs")
 		let { creates, needs } = this.recipes[element];
+		let amount_minus_leftover = amount - this.leftover[element];
+		let need_to_create = Math.max(amount_minus_leftover, 0);
+		let multiplier = Math.ceil(need_to_create / creates);
+		let leftover_after_synthesis = creates * multiplier - amount_minus_leftover;
+		this.leftover[element] = leftover_after_synthesis;
+
+		if (need_to_create === 0) {
+			/**
+			 * We already have enough of this element created, we don't need to synthesize more.
+			 * So, it doesn't cost us any ORE, return 0 to indicate that.
+			 */
+			return 0;
+		}
+
+		// Otherwise, we need to synthesize this element
 		let ore_sum = 0;
 		for (let [piece_element, piece_amount] of needs) {
-			let amount_minus_leftover = amount - this.leftover[piece_element];
-			if (amount_minus_leftover <= 0) {
-				/**
-				 * We already have enough created, we don't need to synthesize more.
-				 * Adjust our leftover and return 0, don't need to spend any more ORE.
-				 */
-				this.leftover[piece_element] -= amount;
-				return 0;
-			}
-
-			let multiplier = Math.ceil(amount_minus_leftover / creates);
-			let remainder = multiplier * creates - amount_minus_leftover;
-
-			this.leftover[piece_element] = remainder;
-
-			ore_sum +=
-				multiplier *
-				this.recursiveCalculateOreTo(piece_element, piece_amount);
+			ore_sum += multiplier * this.recursiveCalculateOreTo(piece_element, piece_amount);
 		}
 
 		return ore_sum;
--- 2019/14/part-one.js
+++ 2019/14/part-one.js
@@ -1,5 +1,8 @@
 const { input, sampleInputs, parseLines } = require('./input');
 const Formula = require('./formula');
 
-let a = new Formula(sampleInputs[0].formula);
+let recipe = sampleInputs[1];
+
+let a = new Formula(recipe.formula);
 console.log(a.calculateOreTo('FUEL', 1));
+console.log(recipe.ore);
