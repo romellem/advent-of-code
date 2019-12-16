60eea095db7576b4d2a90b3601ce2fb9fce2e8d1
romellem <romellem.git@gmail.com>
2019-12-15 19:40:20 -0600

    Comments our formula a lot
    There was a lot going on, annotating as I did helps me maintain my
    understanding.

--- 2019/14/formula.js
+++ 2019/14/formula.js
@@ -1,14 +1,17 @@
 class Formula {
 	constructor(recipes) {
-		this.recipes = recipes;
+		// Clone our recipes
+		this.recipes = JSON.parse(JSON.stringify(recipes));
+		this.leftover;
 	}
 
 	calculateOreTo(element, amount = 1) {
-		// Initialize leftovers to be 0 (including ORE)
+		// Initialize leftovers to be 0
 		this.leftover = Object.keys(this.recipes).reduce(
 			(obj, element) => ((obj[element] = 0), obj),
-			{ ORE: 0 }
+			{}
 		);
+
 		let ore = this.recursiveCalculateOreTo(element, amount);
 
 		return {
@@ -18,29 +21,59 @@ class Formula {
 	}
 
 	recursiveCalculateOreTo(element, amount = 1) {
+		// We don't synthesize ORE, just return that ammount immediately
 		if (element === 'ORE') {
 			return amount;
 		}
 
-		let { creates, needs } = this.recipes[element];
+		// Find our recipe, and see how much that recipe creates as well as the needed ingredients
+		let { creates, needs: ingredients } = this.recipes[element];
+
+		// Subtract our leftovers from how much we need to create
 		let amount_minus_leftover = amount - this.leftover[element];
+
+		// Clamp the amount we need to create at zero (can't create negative elements)
 		let need_to_create = Math.max(amount_minus_leftover, 0);
+
+		// We need to create more, calculate how many times we need to run the recipe
 		let multiplier = Math.ceil(need_to_create / creates);
-		let leftover_after_synthesis = creates * multiplier - amount_minus_leftover;
+
+		// How many would be created after running the recipe `multiplier` times
+		let total_from_running_m_times = creates * multiplier;
+
+		// Add our leftovers to how many we will create
+		let total_plus_leftover = total_from_running_m_times + this.leftover[element];
+
+		// Subtract the amount we need from the amount we've created
+		let leftover_after_synthesis = total_plus_leftover - amount;
+
+		// Save our leftover count, we may not need to synthesize an element later
 		this.leftover[element] = leftover_after_synthesis;
 
 		if (need_to_create === 0) {
 			/**
 			 * We already have enough of this element created, we don't need to synthesize more.
 			 * So, it doesn't cost us any ORE, return 0 to indicate that.
+			 *
+			 * @note that we only do this _after_ we've saved our leftover count. Otherwise our counts will be off.
 			 */
 			return 0;
 		}
 
-		// Otherwise, we need to synthesize this element
+		/**
+		 * If we're here, we don't have enough leftovers to pull from.
+		 * So, we need to sysnthesize them. Recursively calculate the ORE
+		 * cost then for all our ingredients. Importantly, for each of
+		 * the ingredients, multiply it by our multiplier, since that
+		 * is how many of the recipes we need from the prior call.
+		 */
 		let ore_sum = 0;
-		for (let [piece_element, piece_amount] of needs) {
-			ore_sum += this.recursiveCalculateOreTo(piece_element, multiplier * piece_amount);
+
+		for (let [ingredient_element, ingredient_amount] of ingredients) {
+			ore_sum += this.recursiveCalculateOreTo(
+				ingredient_element,
+				multiplier * ingredient_amount
+			);
 		}
 
 		return ore_sum;
