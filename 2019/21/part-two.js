193c15beffea6d9340806fdff9dfc49f55c4e787
romellem <romellem.git@gmail.com>
2019-12-21 11:48:12 -0600

    First obstacle in part two

--- 2019/21/part-two.js
+++ 2019/21/part-two.js
@@ -1,1 +1,48 @@
-const { input } = require('./input');
+const { input: memory } = require('./input');
+const { ASCII, Computer } = require('./intcode-computer');
+
+// NOT N == Hole at N
+
+// Obstacles:
+
+// 1. (0 || (0 || (1 && 1))) && 1 === 1
+//   @...@ 
+// #####.###########
+
+// 1. (1 || (1 || (1 && 1))) && 1 === 1
+//     @...@ 
+// #####...#########
+
+// 1. (0 || (0 || (1 && 1))) && 1 === 1
+// 2. (1 || (0 || (0 && 1))) && 1 === 1
+//    @...@...@
+// #####..#.########
+
+// 1. (0 || (0 || (1 && 1))) && 1 === 1
+// 2. (1 || (1 || (0 && 1))) && 1 === 1
+//   @...@...@
+// #####.#..########
+
+/**
+ * Current program:
+ * (Hole 1 away OR
+ *      (Hole 2 away OR
+ *          (Hole 3 away AND ground 4 away)
+ *      )
+ * ) AND ground 4 away
+ */
+
+let inputs = Computer.parseAsciiInputToArray(
+`NOT C J
+AND D J
+NOT B T
+OR T J
+NOT A T
+OR T J
+AND D J
+RUN
+`);
+
+let ascii = new ASCII(memory, { inputs, pause_on_output: false });
+ascii.run();
+console.log(ascii.computer.flushOutputs());
