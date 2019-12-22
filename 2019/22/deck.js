562af4f7cab8d8e90e5b75bf85116f526fbe6e2c
romellem <romellem.git@gmail.com>
2019-12-22 10:06:41 -0600

    Working part one

--- 2019/22/deck.js
+++ 2019/22/deck.js
@@ -4,7 +4,18 @@ class Deck {
 		this.current_deck = Array(deck_size)
 			.fill()
 			.map((_, i) => i);
-		this.temp_deck = this.deck.slice(0);
+
+		// The values here will be changed later, on init they don't matter
+		this.temp_deck = Array(deck_size).fill();
+	}
+
+	shuffle(position_of_card_to_return = 2019) {
+		for (let { op, arg } of this.moves) {
+			// 'reverse', 'cut', 'increment'
+			this[op](arg);
+		}
+
+		return this.current_deck.indexOf(position_of_card_to_return);
 	}
 
 	reverse() {
@@ -12,10 +23,25 @@ class Deck {
 	}
 
 	cut(n) {
-
+		let size = Math.abs(n);
+		if (n > 0) {
+			let front_part = this.current_deck.splice(0, size);
+			this.current_deck = this.current_deck.concat(front_part);
+		} else {
+			let back_part = this.current_deck.splice(this.current_deck.length - size, size);
+			this.current_deck = back_part.concat(this.current_deck);
+		}
 	}
 
 	increment(n) {
-		
+		for (let c = 0; c < this.current_deck.length; c++) {
+			let position = (c * n) % this.current_deck.length;
+			this.temp_deck[position] = this.current_deck[c];
+		}
+
+		// Copy temp deck into current deck. Temp deck never gets recreated
+		this.current_deck = this.temp_deck.slice(0);
 	}
 }
+
+module.exports = Deck;
--- 2019/22/part-one.js
+++ 2019/22/part-one.js
@@ -1,1 +1,7 @@
 const { input } = require('./input');
+const Deck = require('./deck');
+
+let deck = new Deck(input);
+let output = deck.shuffle();
+
+console.log(output);
