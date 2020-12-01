b3c1d08bae70e1d72c8367a48397d003d02a9943
romellem <romellem.git@gmail.com>
2020-11-30 23:55:48 -0600

    Added more performant code

--- 2020/1/part-one.js
+++ 2020/1/part-one.js
@@ -3,8 +3,8 @@ const G = require('generatorics');
 
 for (let [a, b] of G.combination(input, 2)) {
 	if (a + b === 2020) {
-		console.log(a, b);
-		console.log(a * b);
+		console.log({ a, b });
+		console.log('a * b = ', a * b);
 		return;
 	}
 }
--- 2020/1/part-two.js
+++ 2020/1/part-two.js
@@ -3,8 +3,8 @@ const G = require('generatorics');
 
 for (let [a, b, c] of G.combination(input, 3)) {
 	if (a + b + c === 2020) {
-		console.log(a, b, c);
-		console.log(a * b * c);
+		console.log({ a, b, c });
+		console.log('a * b * c = ', a * b * c);
 		return;
 	}
 }
