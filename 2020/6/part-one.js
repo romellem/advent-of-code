9827367006517090f032cc5c7bf985f2c04871e4
romellem <romellem.git@gmail.com>
2020-12-07 08:36:52 -0600

    Working part one and two

--- 2020/6/part-one.js
+++ 2020/6/part-one.js
@@ -1,3 +1,17 @@
 const { input } = require('./input');
 
-console.log();
+let arr = [];
+for (let lines of input) {
+	let lookup = {};
+	let people = lines.split('\n');
+	for (let p of people) {
+		for (let char of p) {
+			lookup[char] = true;
+		}
+	}
+	arr.push(Object.keys(lookup).length);
+}
+
+let total = arr.reduce((a, b) => a + b, 0);
+
+console.log(total);
