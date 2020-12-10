5a4acf08b53650e56fb4d771766b704bb5e7aa35
romellem <romellem.git@gmail.com>
2020-12-10 09:41:56 -0600

    Looking for a pattern

--- 2020/10/test.js
+++ 2020/10/test.js
@@ -3,7 +3,7 @@ const { Node, Graph } = require('./graph');
 const s = [
     // 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19
     // 1,4,7,8,9,12,14,16
-    1,2
+    1,4,5,6,9,12,14,15,17
     // 1,4,7,10,12,14,16
     // 1
     //  2,3,5
@@ -12,18 +12,21 @@ const s = [
 
 
 function reduceInput(o) {
+    return o;
     // debugger;
     let input = o.slice(0);
     let reduced = [0];
-    let skipped;
-    for (let i = input.length - 1; i > 1 ; i--) {
+    let next_interval;
+    for (let i = 0; i< input.length; i++) {
         let a = reduced[reduced.length - 1]
         let b = input[i]
         let c = input[i+1]
-        if (a - c > 3) {
-            
+        if (c - a > 3) {
+            if (!next_interval)
+                next_interval = b - a;
         } else {
-            reduced.push(a);
+            reduced.push(a + next_interval);
+            next_interval = undefined;
         }
     }
     reduced.push(input[1]);
@@ -31,8 +34,8 @@ function reduceInput(o) {
     return reduced;
 }
 
-let r = reduceInput(s);
-r.shift();
-console.log(r)
+// let r = reduceInput(s);
+// r.shift();
+// console.log(r)
 let g = new Graph(s);
 console.log(g.print());
