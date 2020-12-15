3154f95735c930a50c7e4af16cbb318587d4ea8f
romellem <romellem.git@gmail.com>
2020-12-15 08:37:38 -0600

    Moves functions to exports

--- 2020/9/part-one.js
+++ 2020/9/part-one.js
@@ -1,25 +1,4 @@
 const { input } = require('./input');
-const G = require('generatorics');
+const { partOne } = require('./encoding');
 
-const hasSum = (num, list) => {
-	for (let [x, y] of G.combination(list, 2)) {
-		if (x + y === num) {
-			return true;
-		}
-	}
-	return false;
-};
-
-function partOne(input) {
-	const JUMP = 25;
-	for (let i = JUMP; i < input.length - 1; i++) {
-		let num = input[i];
-		let prev = input.slice(i - JUMP, i);
-		if (!hasSum(num, prev)) {
-			return num;
-		}
-	}
-}
-
-const part_one_answer = partOne(input);
-console.log(part_one_answer);
+console.log(partOne(input));
