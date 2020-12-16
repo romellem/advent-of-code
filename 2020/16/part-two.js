b7f23560a0ec2e0769e258378768d93964a9fb66
romellem <romellem.git@gmail.com>
2020-12-16 10:28:47 -0600

    Working part two!
    Pretty happy with how this one turned out.

--- 2020/16/part-two.js
+++ 2020/16/part-two.js
@@ -1,6 +1,16 @@
 const { rules, your_ticket, nearby_tickets } = require('./input');
-const { getInvalidValueFromTicket } = require('./ticket');
+const { getInvalidValueFromTicket, determineOrderingOfTicketFields } = require('./ticket');
 
 const valid_tickets = nearby_tickets.filter(
 	(ticket) => getInvalidValueFromTicket(rules, ticket) === undefined
 );
+
+let ordering = determineOrderingOfTicketFields(rules, valid_tickets);
+
+let your_ticket_departure_fields = your_ticket.filter((value, field_index) => {
+	let { name: field_name } = ordering[field_index];
+	return field_name.startsWith('departure');
+});
+
+let your_ticket_departure_fields_product = your_ticket_departure_fields.reduce((a, b) => a * b, 1);
+console.log(your_ticket_departure_fields_product);
