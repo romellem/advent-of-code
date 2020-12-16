60e59c73651fcd0e9106c2a323c2b274e0cd03f5
romellem <romellem.git@gmail.com>
2020-12-16 09:09:27 -0600

    Filters for valid tickets

--- 2020/16/part-two.js
+++ 2020/16/part-two.js
@@ -1,1 +1,6 @@
-const { input } = require('./input');
+const { rules, your_ticket, nearby_tickets } = require('./input');
+const { getInvalidValueFromTicket } = require('./ticket');
+
+const valid_tickets = nearby_tickets.filter(
+	(ticket) => getInvalidValueFromTicket(rules, ticket) === undefined
+);
