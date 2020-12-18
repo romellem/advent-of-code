12d2a4e0497307a0b52e4fa0e96269791fd09d05
romellem <romellem.git@gmail.com>
2020-12-17 23:38:01 -0600

    Removes test code

--- 2020/19/part-one.js
+++ 2020/19/part-one.js
@@ -2,13 +2,13 @@ const { input } = require('./input');
 const { calculate } = require('./calc');
 
 
-for (let s of [
-    '2 * 3 + (4 * 5)',
-    '5 + (8 * 3 + 9 + 3 * 4 * 3)',
-    '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
-    '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
-]) {
-    console.log(s, '=', calculate(s))
-}
-console.log('===========')
+// for (let s of [
+//     '2 * 3 + (4 * 5)',
+//     '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+//     '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+//     '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+// ]) {
+//     console.log(s, '=', calculate(s))
+// }
+// console.log('===========')
 console.log(input.map(v => calculate(v)).reduce((a, b) => a+b, 0))
