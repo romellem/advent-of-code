3cfe1e13fd910ff4990e2091e7feefa2c240e704
romellem <romellem.git@gmail.com>
2020-12-17 23:39:01 -0600

    Working part two

--- 2020/19/calc.js
+++ 2020/19/calc.js
@@ -54,6 +54,46 @@ function calculate(input) {
 
 }
 
+function calculate2(input) {
+	input = input.trim();
+	if (!input) {
+		return;
+	}
+
+	input = input.split('(').join('( ');
+	input = input.split(')').join(' )');
+
+	let tokens = input.split(' ').map(v => /\d+/.test(v) ? parseInt(v, 10) : v);
+
+	while (tokens.includes(')')) {
+		let close_paren = tokens.indexOf(')');
+		let open_paren = indexOfFrom('(', tokens, close_paren);
+		let slice = tokens.slice(open_paren + 1, close_paren);
+		const orig_length = slice.length;
+
+		while (slice.includes('+')) {
+			let plus = slice.indexOf('+');
+			let sum = slice[plus - 1] + slice[plus + 1];
+			slice.splice(plus - 1, 3, sum);
+		}
+
+		let total = reduce(slice);
+		tokens.splice(open_paren, orig_length + 2, total)
+	}
+
+	while (tokens.includes('+')) {
+		let plus = tokens.indexOf('+');
+		let sum = tokens[plus - 1] + tokens[plus + 1];
+		tokens.splice(plus - 1, 3, sum);
+	}
+
+	let total = reduce(tokens);
+	return total;
+
+
+}
+
+
 function indexOfFrom(char, arr, from) {
 	for (let i = from; i >= 0; i--) {
 		if (arr[i] === char) return i;
@@ -74,6 +114,22 @@ function reduce(tokens) {
 	return acc;
 }
 
+function reduce2(tokens) {
+	
+	let acc = tokens[0];
+	for (let i = 1; i < tokens.length; i += 2) {
+		let operator = tokens[i];
+		let digit = tokens[i + 1];
+		if (!OPERATORS[operator]) {
+			throw new Error(`Unknown operator: ${operator}`);
+		}
+		acc = OPERATORS[operator](acc, digit);
+	}
+
+	return acc;
+}
+
 module.exports = {
 	calculate,
+	calculate2,
 }
--- 2020/19/part-two.js
+++ 2020/19/part-two.js
@@ -1,1 +1,14 @@
 const { input } = require('./input');
+const { calculate2 } = require('./calc');
+
+
+// for (let s of [
+//     '2 * 3 + (4 * 5)',
+//     '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+//     '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+//     '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+// ]) {
+//     console.log(s, '=', calculate(s))
+// }
+// console.log('===========')
+console.log(input.map(v => calculate2(v)).reduce((a, b) => a+b, 0))
