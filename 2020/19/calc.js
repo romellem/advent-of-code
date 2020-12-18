c66dade8c8af1ed283b1fca4e57fdaa81a6d3625
romellem <romellem.git@gmail.com>
2020-12-17 23:37:01 -0600

    Fixes bug, don't modify array
    Otherwise, later referencing `array.length` later is off by one!

--- 2020/19/calc.js
+++ 2020/19/calc.js
@@ -61,8 +61,8 @@ function indexOfFrom(char, arr, from) {
 }
 
 function reduce(tokens) {
-	let acc = tokens.shift();
-	for (let i = 0; i < tokens.length; i += 2) {
+	let acc = tokens[0];
+	for (let i = 1; i < tokens.length; i += 2) {
 		let operator = tokens[i];
 		let digit = tokens[i + 1];
 		if (!OPERATORS[operator]) {
