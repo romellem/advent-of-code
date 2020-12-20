735e853733299c7113caa76ebbf73448a8678512
romellem <romellem.git@gmail.com>
2020-12-20 12:03:23 -0600

    Continues work
    Getting close, need to implement the `row` and `col`
    functions against the square strings

--- 2020/20/puzzle.js
+++ 2020/20/puzzle.js
@@ -1,10 +1,23 @@
+const { uniq } = require('lodash');
 const G = require('generatorics');
 
+const TOP = 'top';
+const LEFT = 'left';
+const RIGHT = 'right';
+const BOTTOM = 'bottom';
+const SIDE_COMPLEMENT = {
+	[TOP]: BOTTOM,
+	[LEFT]: RIGHT,
+	[RIGHT]: LEFT,
+	[BOTTOM]: TOP,
+};
+const SIDES = Object.keys(SIDE_COMPLEMENT);
+
 /**
- * Rotates a square 2d array 90 degrees clockwise in place
+ * Rotates a square 2d array 90 degrees clockwise in place.
  * @link https://code.likeagirl.io/rotate-an-2d-matrix-90-degree-clockwise-without-create-another-array-49209ea8b6e6
  * @param {Array<Array>} matrix
- * @returns {Array<Array>}
+ * @returns {Array<Array>} - Returns the reference to our original matrix
  */
 function rotate(matrix) {
 	const n = matrix.length;
@@ -26,7 +39,7 @@ function rotate(matrix) {
 /**
  * Flips a matrix in the `y` direction in place.
  * @param {Array<Array>} matrix
- * @returns {Array<Array>}
+ * @returns {Array<Array>} - Returns the reference to our original matrix
  */
 function flipY(matrix) {
 	const n = matrix.length;
@@ -40,32 +53,99 @@ function flipY(matrix) {
 	return matrix;
 }
 
-function picture(squares) {}
+/**
+ * Flips a matrix in the `x` direction in place.
+ * This can also be achieved by rotating 180 degrees
+ * and flipping in the `y` direction, so this method
+ * goes unused in our code below. Including for
+ * completeness sake.
+ *
+ * @param {Array<Array>} matrix
+ * @returns {Array<Array>} - Returns the reference to our original matrix
+ */
+function flipX(matrix) {
+	const n = matrix.length;
+	const x = matrix[0].length;
+	const half = Math.floor(x / 2);
+	for (let i = 0; i < half; i++) {
+		for (let j = 0; j < n; j++) {
+			let t = matrix[j][i];
+			matrix[j][i] = matrix[j][n - (i + 1)];
+			matrix[j][n - (i + 1)] = t;
+		}
+	}
 
-class Piece {
+	return matrix;
+}
+
+class PuzzlePiece {
 	constructor(piece) {
 		this.piece_str = JSON.stringify(piece);
 		this.piece = JSON.parse(this.piece_str);
+		this.orientations = this.generateOrientations();
+		this.edge_length = this.piece.length;
 
-		this.top = [];
-		this.left = [];
-		this.right = [];
-		this.bottom = [];
+		this.top = new Set();
+		this.left = new Set();
+		this.right = new Set();
+		this.bottom = new Set();
 	}
 
-	row(i) {
-		let str = '';
-		for (let x = 0; x < this.piece[i].length; x++) {
-			str += this.piece[i][x];
+	/**
+	 * @returns {Array<String>} - Returns an array of unique orientations for the puzzle piece.
+	 */
+	generateOrientations() {
+		let orientations = [];
+		for (let i = 0; i < 4; i++) {
+			rotate(this.piece);
+			orientations.push(this.piece.toString());
+			flipY(this.piece);
+			orientations.push(this.piece.toString());
 		}
-		return str;
+
+		return uniq(orientations);
 	}
-	col(i) {
-		let str = '';
-		for (let y = 0; y < this.piece.length; y++) {
-			str += this.piece[y][i];
+
+	getEdge(side) {
+		switch (side) {
+			case TOP:
+				return this.row(0);
+			case BOTTOM:
+				return this.row(this.outer_index);
+			case LEFT:
+				return this.col(0);
+			case RIGHT:
+				return this.col(this.outer_index);
 		}
-		return str;
+	}
+
+	row(i, square_str) {
+		if (square_str) {
+		} else {
+			return this.piece[i].join('');
+		}
+	}
+	col(i, square_str) {
+		if (square_str) {
+		} else {
+			let str = '';
+			for (let y = 0; y < this.piece.length; y++) {
+				str += this.piece[y][i];
+			}
+			return str;
+		}
+	}
+
+	fit(other_piece, side) {
+		const other_side = SIDE_COMPLEMENT[side];
+		this[side].add(other_piece);
+		other_piece[other_side].add(this);
+	}
+
+	tryToFit(other_piece) {
+		for (let side of SIDES) {
+			let other_side = SIDE_COMPLEMENT[side];
+		}
 	}
 
 	canFit(other_piece) {
@@ -107,8 +187,11 @@ class Piece {
 		return can_fit;
 	}
 
+	/**
+	 * @returns {String} - Returns string of _current_ piece orientation.
+	 */
 	toString() {
-		return this.piece_str;
+		return this.piece.map((row) => row.join('')).join('\n');
 	}
 }
 
