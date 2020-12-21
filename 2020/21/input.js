a8e3a9267139cebdc4e46af16e88603758764440
romellem <romellem.git@gmail.com>
2020-12-21 15:51:51 -0600

    Input parsing

--- 2020/21/input.js
+++ 2020/21/input.js
@@ -5,7 +5,26 @@ const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
-	.split('\n');
+	.split('\n')
+	.map((line) => {
+		/**
+		 * @example mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+		 * @example trh fvjkl sbzzf mxmxvkd (contains dairy)
+		 */
+		let [ingredients] = line.split(' (contains');
+		ingredients = ingredients.split(' ');
+		// let ingredients_lookup = ingredients.reduce((obj, v) => ((obj[v] = true), obj), {});
+		let [, allergens] = /contains ([\w,\s]+)\)/.exec(line);
+		allergens = allergens.split(', ');
+		// let allergens_lookup = allergens.reduce((obj, v) => ((obj[v] = true), obj), {});
+
+		return {
+			ingredients,
+			// ingredients_lookup,
+			allergens,
+			// allergens_lookup,
+		};
+	});
 
 module.exports = {
 	input,
