0e7ab4c4836a58b4719cee181c82d90ebbec3f2a
romellem <romellem.git@gmail.com>
2020-12-21 23:51:10 -0600

    More readable code

--- 2020/22/game.js
+++ 2020/22/game.js
@@ -16,13 +16,24 @@ function play(p1, p2) {
 	return score;
 }
 
+const PLAYER_1 = '1';
+const PLAYER_2 = '2';
+
 function recursivePlay(p1, p2, depth = 0) {
-    // process.stdout.write(depth + '\r')
 	let played = {};
 	while (p1.length && p2.length) {
 		let check = p1.join('') + '+' + p2.join('');
-		if (played[check]) {
-			return 'p1';
+
+		/**
+		 * Before either player deals a card, if there was a previous round
+		 * in this game that had exactly the same cards in the same order
+		 * in the same players' decks, the game instantly ends in a win for
+		 * player 1. Previous rounds from other games are not considered.
+		 * (This prevents infinite games of Recursive Combat, which everyone
+		 * agrees is a bad idea.)
+		 */
+		if (played[check] === true) {
+			return PLAYER_1;
 		}
 		played[check] = true;
 
@@ -31,30 +42,26 @@ function recursivePlay(p1, p2, depth = 0) {
 
 		if (p1_top <= p1.length && p2_top <= p2.length) {
 			let winner = recursivePlay(p1.slice(0, p1_top), p2.slice(0, p2_top), depth + 1);
-			if (winner === 'p1') {
+			if (winner === PLAYER_1) {
 				p1.push(p1_top, p2_top);
-			} else if (winner === 'p2') {
+			} else if (PLAYER_2) {
 				p2.push(p2_top, p1_top);
-			} else {
-				throw winner;
 			}
 		} else if (p1_top > p2_top) {
 			p1.push(p1_top, p2_top);
 		} else if (p1_top < p2_top) {
 			p2.push(p2_top, p1_top);
-		} else {
-            throw [p1, p2]
-        }
+		}
 	}
 
+	// Only compute score on initial game as an optimization
 	if (depth === 0) {
 		let winner = p1.length ? p1.reverse() : p2.reverse();
 		let score = winner.map((v, i) => v * (i + 1)).reduce((a, b) => a + b, 0);
 
-        console.log('=========\n=======')
 		return score;
 	} else {
-		return p1.length ? 'p1' : 'p2';
+		return p1.length ? PLAYER_1 : PLAYER_2;
 	}
 }
 
