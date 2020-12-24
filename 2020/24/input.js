6d75b1ed86ce079cc46f4b421c2a8fa33c7be753
romellem <romellem.git@gmail.com>
2020-12-23 23:22:13 -0600

    Parses input, not working

--- 2020/24/input.js
+++ 2020/24/input.js
@@ -7,7 +7,34 @@ const input = fs
 	.trim()
 	.split('\n')
 	.map((line) => {
-		return line;
+		let a = line.split('se').join(',se,');
+		a = line.split('sw').join(',sw,');
+		a = line.split('nw').join(',nw,');
+		a = line.split('ne').join(',ne,');
+		// console.log(a);
+		// process.exit()
+		let paths = a.split(',').filter(v=>v);
+		// console.log(paths);
+		// process.exit()
+
+		const dirs =  {
+			'nw':1,
+			'ne':1,
+			'sw':1,
+			'se':1,
+			'e':1,
+			'w':1,
+		};
+		paths = paths.map(p => {
+			if (!dirs[p]) {
+				return p.split('')
+			} else {
+				return p
+			}
+		}).flat();
+		// console.log(paths);
+		// process.exit()
+		return paths
 	});
 
 module.exports = {
