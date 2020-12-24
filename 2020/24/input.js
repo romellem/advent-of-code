8f3e2ef2168c50dc2d6ab2f5b6297e6121075641
romellem <romellem.git@gmail.com>
2020-12-23 23:24:10 -0600

    Formatting for input parsing

--- 2020/24/input.js
+++ 2020/24/input.js
@@ -1,6 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 
+const dirs = {
+	nw: 1,
+	ne: 1,
+	sw: 1,
+	se: 1,
+	e: 1,
+	w: 1,
+};
+
 const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
@@ -11,30 +20,21 @@ const input = fs
 		a = a.split('sw').join(',sw,');
 		a = a.split('nw').join(',nw,');
 		a = a.split('ne').join(',ne,');
-		// console.log(a);
-		// process.exit()
-		let paths = a.split(',').filter(v=>v);
-		// console.log(paths);
-		// process.exit()
 
-		const dirs =  {
-			'nw':1,
-			'ne':1,
-			'sw':1,
-			'se':1,
-			'e':1,
-			'w':1,
-		};
-		paths = paths.map(p => {
-			if (!dirs[p]) {
-				return p.split('')
-			} else {
-				return p
-			}
-		}).flat();
-		// console.log(paths);
-		// process.exit()
-		return paths
+		let paths = a.split(',').filter((v) => v);
+
+		paths = paths
+			.map((p) => {
+				if (!dirs[p]) {
+					// Has to be `ee`+ or `ww`+
+					return p.split('');
+				} else {
+					return p;
+				}
+			})
+			.flat();
+
+		return paths;
 	});
 
 module.exports = {
