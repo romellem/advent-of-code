7672b855c03e1b00b19389918ebf0621cfe6b57d
romellem <romellem.git@gmail.com>
2021-01-01 22:15:19 -0800

    Fixes part two
    Not sure how this was different from before, but :shrug:
    
    Seems more readable now, and I believe a bit faster.

--- 2020/24/part-two.js
+++ 2020/24/part-two.js
@@ -57,30 +57,43 @@ function getUniqueNeighborsOfCoordsStr(coords, color) {
 	return uniq(coords.map((coord) => getNeighborsOfCoordStr(coord, color)).flat());
 }
 
+function getNeighborColorCount(coord, color) {
+	let count = 0;
+	let cell = getHexFromCoordStr(coord);
+	for (let dir of DIAGONALS) {
+		let neighbor = cell.add(Hex.diagonals[dir]);
+		let neighbor_str = neighbor.toString();
+		let neighbor_color = getGridColor(neighbor_str);
+		if (neighbor_color === color) {
+			count++;
+		}
+	}
+	return count;
+}
+
 for (let iterations = 0; iterations < 100; iterations++) {
+	// Keeps memory low, and prevents us from iterating over white tiles
 	pruneZeroes();
+
 	let tiles = Object.keys(GRID);
 
 	let black_tiles_to_flip = [];
 	for (let tile of tiles) {
-		let black_neighbors = getNeighborsOfCoordStr(tile, 1);
+		let black_neighboar_tiles_count = getNeighborColorCount(tile, 1);
 
-		if (black_neighbors.length === 0 || black_neighbors.length > 2) {
+		if (black_neighboar_tiles_count === 0 || black_neighboar_tiles_count > 2) {
 			black_tiles_to_flip.push(tile);
 		}
 	}
 
-	let white_neighbors = getUniqueNeighborsOfCoordsStr(tiles);
+	let white_neighbor_tiles = getUniqueNeighborsOfCoordsStr(tiles, 0);
 
 	let white_tiles_to_flip = [];
-	for (let white_neighbor of white_neighbors) {
-		let black_tiles_count = getNeighborsOfCoordStr(white_neighbor, 0).reduce(
-			(sum, coord) => sum + (GRID[coord] === 1 ? 1 : 0),
-			0
-		);
+	for (let white_neighbor_tile of white_neighbor_tiles) {
+		let black_neighbor_tiles_count = getNeighborColorCount(white_neighbor_tile, 1);
 
-		if (black_tiles_count === 2) {
-			white_tiles_to_flip.push(white_neighbor);
+		if (black_neighbor_tiles_count === 2) {
+			white_tiles_to_flip.push(white_neighbor_tile);
 		}
 	}
 
