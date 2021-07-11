8af0a76b4f01f6632cc69d4c47d614c6655bdf0a
romellem <romellem.git@gmail.com>
2021-07-11 16:24:15 -0500

    Removes frames output

--- 2018/17/part-one.js
+++ 2018/17/part-one.js
@@ -3,6 +3,6 @@ const { Grid, Ground } = require('./ground.js');
 
 const grid = new Grid(input);
 const ground = new Ground({ grid });
-ground.fill(true).then((sums) => {
+ground.fill().then((sums) => {
 	console.log(sums[Ground.FLOWING] + sums[Ground.SETTLED]);
 });
