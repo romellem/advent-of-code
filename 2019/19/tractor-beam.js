c70ad7d43cde6d6f93048876d1516f7ae48c27f8
romellem <romellem.git@gmail.com>
2021-07-16 12:51:31 -0500

    Renames computer

--- 2019/19/tractor-beam.js
+++ 2019/19/tractor-beam.js
@@ -1,4 +1,4 @@
-const { Computer } = require('./intcode-computer.js');
+const { Computer } = require('./intcode-computer-optimized.js');
 const { InfiniteGrid } = require('./infinite-grid.js');
 
 class TractorBeam {
