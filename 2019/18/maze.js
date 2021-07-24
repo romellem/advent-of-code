cd497f32c421aea5df76529ccec4e2933544b1af
romellem <romellem.git@gmail.com>
2021-07-24 11:56:09 -0500

    Begins count and pathfinder in Maze class

--- 2019/18/maze.js
+++ 2019/18/maze.js
@@ -7,12 +7,22 @@ const WALL = '#';
 const DOOR_RE = /[A-Z]/;
 const KEY_RE = /[a-z]/;
 
+// Works for our input
+const cloneJSON = (obj) => JSON.parse(JSON.stringify(obj));
+
 class Maze {
-	constructor(raw_input) {
+	constructor(raw_input, auto_create_pathfinders = true) {
 		this.grid = Maze.parseInput(raw_input);
 		this.entrances = this.grid.findAll(ENTRANCE);
 		this.doors = this.grid.findAll(DOOR_RE);
 		this.keys = this.grid.findAll(KEY_RE);
+
+		// By keys_collected, then by from/to
+		this.count_cache = {};
+
+		if (auto_create_frontiers) {
+			this.pathfinders = this.generatePathfinders();
+		}
 	}
 
 	static parseInput(raw_input) {
@@ -59,42 +69,45 @@ class Maze {
 		this.entrances = this.grid.findAll(ENTRANCE);
 	}
 
-	getShortestPath() {
-		const makePath = ({ x, y, steps = 0, keys_collected = '' } = {}) => ({
-			x,
-			y,
-			steps,
-			keys_collected,
-			at_end: false,
-		});
+	/**
+	 * I am being paranoid and only memoizing by adding an extra caching
+	 * layer by keys_collected, just in case new paths open up as new doors
+	 * get unlocked.
+	 */
+	countSteps(from_id, to_coord, keys_collected) {
+		let current = to_coord;
+		let count = 0;
+		if (!this.count_cache[keys_collected]) {
+			this.count_cache[keys_collected] = {};
+		}
+		const came_from = this.pathfinders.get(from_id);
+		const cache_key = `${InfiniteGrid.toId(...current)}/${from_id}`;
+		if (this.count_cache[keys_collected][cache_key] === undefined) {
+			while (InfiniteGrid.toId(...current) !== from_id) {
+				count++;
+				current = came_from.get(InfiniteGrid.toId(...current));
+			}
+			this.count_cache[keys_collected][cache_key] = count;
+		}
 
-		// paths = [{ x, y, steps, keys_collected }, ...]
-		let paths = [...this.entrances.values()].map(([x, y]) => makePath({ x, y }));
+		return this.count_cache[keys_collected][cache_key];
+	}
+
+	getShortestPath() {
+		const entrances_coords = [...this.entrances.values()];
+
+		let paths = [
+			{
+				keys_collected: '',
+				at_end: false,
+				steps: 0,
+				// [[x1, y1], [x2, y2], ...]
+				robots_coords: cloneJSON(entrances_coords),
+			},
+		];
 		while (paths.some((path) => !path.at_end)) {
 			// Memo this function based on from / to coords
-			const countSteps = (() => {
-				const cache = {};
-				return (from_id, to_coord, came_from) => {
-					let current = to_coord;
-					let count = 0;
-					const cache_key = `${InfiniteGrid.toId(
-						current[0],
-						current[1]
-					)}/${from_id}`;
-
-					if (cache[cache_key] === undefined) {
-						while (InfiniteGrid.toId(current[0], current[1]) !== from_id) {
-							count++;
-							current = came_from.get(
-								InfiniteGrid.toId(current[0], current[1])
-							);
-						}
-						cache[cache_key] = count;
-					}
-
-					return cache[cache_key];
-				};
-			})();
+			const countSteps = makeCountSteps();
 
 			let new_paths = [];
 			console.log(`${paths[0].keys_collected.length} / ${this.keys.size}`);
@@ -188,6 +201,44 @@ class Maze {
 		}
 
 		return paths;
+	}
+
+	generatePathfinders() {
+		const pathfinders = new Map();
+		for (let [x, y] of this.keys.values()) {
+			const frontier = new Queue();
+			frontier.push([x, y]);
+			const came_from = new Map([[InfiniteGrid.toId(x, y), null]]);
+			while (!frontier.isEmpty()) {
+				const current_coord = frontier.shift();
+				const neighbor_coords = this.grid.neighbors(...current_coord).values();
+				for (let { coord: next_coord, value: next_cell } of neighbor_coords) {
+					if (next_cell === WALL) continue;
+
+					const next_id = InfiniteGrid.toId(...next_coord);
+					if (came_from.has(next_id)) continue;
+
+					// Coord is walkable
+					const is_walkable =
+						next_cell === PASSAGE ||
+						next_cell === ENTRANCE ||
+						KEY_RE.test(next_cell) ||
+						DOOR_RE.test(next_cell);
+
+					if (is_walkable) {
+						frontier.push(next_coord);
+					} else {
+						throw new Error(`Unknown cell: ${next_cell}`);
+					}
+
+					came_from.set(next_id, current_coord);
+				}
+			}
+
+			pathfinders.set(InfiniteGrid.toId(x, y), came_from);
+		}
+
+		return pathfinders;
 	}
 }
 
