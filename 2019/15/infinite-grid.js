766f8da5ea946e8a7d18d6006cc52f60c2b4fb41
romellem <romellem.git@gmail.com>
2021-07-26 07:38:49 -0500

    Organizes methods

--- 2019/15/infinite-grid.js
+++ 2019/15/infinite-grid.js
@@ -26,7 +26,56 @@ class InfiniteGrid {
 	}
 
 	static split(two_dimensional_string) {
-		return two_dimensional_string.split('\n').map(row => row.split(''));
+		return two_dimensional_string.split('\n').map((row) => row.split(''));
+	}
+
+	clone() {
+		const infinite_grid_clone = new InfiniteGrid();
+		const new_map = new Map();
+		for (let [key, val] of this.grid) {
+			new_map.set(
+				key,
+				typeof val === 'object' ? JSON.parse(JSON.stringify(val)) : val
+			);
+		}
+		infinite_grid_clone.defaultFactory = this.defaultFactory.bind(this);
+		infinite_grid_clone.string_map = Object.assign({}, this.string_map);
+		infinite_grid_clone.grid = new_map;
+		infinite_grid_clone.max_x = this.max_x;
+		infinite_grid_clone.min_x = this.min_x;
+		infinite_grid_clone.max_y = this.max_y;
+		infinite_grid_clone.min_y = this.min_y;
+
+		return infinite_grid_clone;
+	}
+
+	/**
+	 * @param {RegExp|any} value
+	 * @param {Boolean} [as_coords] - When true, returns a Map of `[x, y]` number values, otherwise returns a Map of string IDs.
+	 * @returns {Array<[any,Id|Coord]>} - Returns an Array, the first value matching the cell found, and the 2nd the coords or ID.
+	 */
+	findAll(value, as_coords = true) {
+		const found = [];
+		for (let [id, cell] of this.grid) {
+			const check = value instanceof RegExp ? value.test(cell) : value === cell;
+			if (check) {
+				found.push([cell, as_coords ? InfiniteGrid.toCoords(id) : id]);
+			}
+		}
+
+		return found;
+	}
+
+	get(x, y) {
+		const id = InfiniteGrid.toId(x, y);
+		if (!this.grid.has(id)) {
+			this.set(x, y, this.defaultFactory(x, y));
+		}
+		return this.grid.get(id);
+	}
+
+	inBounds(x, y) {
+		return x >= this.min_x && x <= this.max_x && y >= this.min_y && y <= this.max_y;
 	}
 
 	/**
@@ -57,6 +106,14 @@ class InfiniteGrid {
 		return neighboring_cells;
 	}
 
+	reset() {
+		this.grid = new Map();
+		this.max_x = -Infinity;
+		this.min_x = Infinity;
+		this.max_y = -Infinity;
+		this.min_y = Infinity;
+	}
+
 	set(x, y, value) {
 		if (typeof x !== 'number' || typeof y !== 'number') {
 			throw new Error(
@@ -71,50 +128,13 @@ class InfiniteGrid {
 		this.grid.set(id, value);
 	}
 
-	get(x, y) {
-		const id = InfiniteGrid.toId(x, y);
-		if (!this.grid.has(id)) {
-			this.set(x, y, this.defaultFactory(x, y));
-		}
-		return this.grid.get(id);
-	}
-
-	/**
-	 * @param {RegExp|any} value
-	 * @param {Boolean} [as_coords] - When true, returns a Map of `[x, y]` number values, otherwise returns a Map of string IDs.
-	 * @returns {Array<[any,Id|Coord]>} - Returns an Array, the first value matching the cell found, and the 2nd the coords or ID.
-	 */
-	findAll(value, as_coords = true) {
-		const found = [];
-		for (let [id, cell] of this.grid) {
-			const check = value instanceof RegExp ? value.test(cell) : value === cell;
-			if (check) {
-				found.push([cell, as_coords ? InfiniteGrid.toCoords(id) : id]);
-			}
+	sum() {
+		let sum = 0;
+		for (let value of this.grid.values()) {
+			sum += value;
 		}
 
-		return found;
-	}
-
-	inBounds(x, y) {
-		return x >= this.min_x && x <= this.max_x && y >= this.min_y && y <= this.max_y;
-	}
-
-	clone() {
-		const infinite_grid_clone = new InfiniteGrid();
-		const new_map = new Map();
-		for (let [key, val] of this.grid) {
-			new_map.set(key, typeof val === 'object' ? JSON.parse(JSON.stringify(val)) : val);
-		}
-		infinite_grid_clone.defaultFactory = this.defaultFactory.bind(this);
-		infinite_grid_clone.string_map = JSON.parse(JSON.stringify(this.string_map));
-		infinite_grid_clone.grid = new_map;
-		infinite_grid_clone.max_x = this.max_x;
-		infinite_grid_clone.min_x = this.min_x;
-		infinite_grid_clone.max_y = this.max_y;
-		infinite_grid_clone.min_y = this.min_y;
-
-		return infinite_grid_clone;
+		return sum;
 	}
 
 	toGrid() {
@@ -129,15 +149,6 @@ class InfiniteGrid {
 		}
 
 		return grid;
-	}
-
-	sum() {
-		let sum = 0;
-		for (let value of this.grid.values()) {
-			sum += value;
-		}
-
-		return sum;
 	}
 
 	toString() {
