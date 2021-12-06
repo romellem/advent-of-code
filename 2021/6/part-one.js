5cc3333f60d7560e061f17e7ed40338bdd44907e
romellem <romellem.git@gmail.com>
2021-12-06 00:06:46 -0600

    Adds working part one and two
    Part two took me a bit, had to optimize otherwise you run out of memory
    trying to do it the "naive" way from part one.

--- 2021/6/part-one.js
+++ 2021/6/part-one.js
@@ -1,5 +1,18 @@
-const { input } = require('./input');
+const { input } = require("./input");
 
-let answer;
+let fishes = [...input];
 
-console.log(answer);
+for (let i = 0; i < 80; i++) {
+	let new_fishes = [];
+	for (let f = 0; f < fishes.length; f++) {
+		fishes[f]--;
+		if (fishes[f] < 0) {
+			new_fishes.push(8);
+			fishes[f] = 6;
+		}
+	}
+
+	fishes.push(...new_fishes);
+}
+
+console.log(fishes.length);
