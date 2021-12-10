ef81c31eb96afaff7ee200a328822aa9aac07692
romellem <romellem.git@gmail.com>
2021-12-10 09:07:14 -0600

    Saves more debugging, still not sure where things are going wrong

--- 2021/10/part-two.js
+++ 2021/10/part-two.js
@@ -1,11 +1,12 @@
 const { input } = require('./input');
-const { score, getLineStatus, chars, INCOMPLETE_SCORE } = require('./syntax');
+const { getLineStatus, chars, INCOMPLETE_SCORE } = require('./syntax');
 
 const incomplete_scores = input
 	.reduce((scores, line) => {
 		const status = getLineStatus(line);
 		if (status.incomplete) {
 			const completion_string = status.stack.map((c) => chars[c]);
+			// console.log(completion_string.join(''));
 			const completion_score = completion_string.reduce((sum, char) => {
 				sum *= 5;
 				sum += INCOMPLETE_SCORE[char];
@@ -18,6 +19,30 @@ const incomplete_scores = input
 	}, [])
 	.sort((a, b) => a - b);
 
+// 942679657 is too low
 const middle_score = incomplete_scores[Math.floor(incomplete_scores.length / 2)];
 
-console.log(middle_score);
+// console.log(middle_score);
+
+function scoreIncomplete(remaining) {
+	let a = remaining.split('').reduce((sum, char) => {
+		sum *= 5;
+		sum += INCOMPLETE_SCORE[char];
+		return sum;
+	}, 0);
+
+	console.log(a);
+	return a;
+}
+
+let sss = [
+	scoreIncomplete('}}]])})]'), // 288957 total points.
+	scoreIncomplete(')}>]})'), // 5566 total points.
+	scoreIncomplete('}}>}>))))'), // 1480781 total points.
+	scoreIncomplete(']]}}]}]}>'), // 995444 total points.
+	scoreIncomplete('])}>'),
+].sort((a, b) => a - b); // 294 total points.
+
+const mm = sss[Math.floor(sss.length / 2)];
+
+console.log(mm);
