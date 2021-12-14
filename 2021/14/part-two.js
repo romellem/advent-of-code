48b94af9b764f4ef4caed5eec38aa55a882d47ad
romellem <romellem.git@gmail.com>
2021-12-13 23:12:56 -0600

    Copies into part two

--- 2021/14/part-two.js
+++ 2021/14/part-two.js
@@ -1,1 +1,59 @@
-const { input } = require('./input');
+const { poly, rules } = require('./input');
+
+class Element {
+	constructor(name) {
+		this.name = name;
+		this.next = null;
+	}
+
+	toString() {
+		return this.name;
+	}
+
+	reset() {
+		this.next = null;
+	}
+}
+
+let list = poly.split('').map((v) => new Element(v));
+
+for (let i = 0; i < 10; i++) {
+	console.log(list.join(''));
+	for (let j = 0; j < list.length - 1; j++) {
+		let a = list[j];
+		let b = list[j + 1];
+
+		let pair = '' + a + b;
+
+		if (rules.has(pair)) {
+			a.next = new Element(rules.get(pair));
+		}
+	}
+
+	let new_list = [];
+	for (let j = 0; j < list.length; j++) {
+		let a = list[j];
+		new_list.push(a);
+
+		if (a.next) {
+			new_list.push(a.next);
+			a.reset();
+		}
+	}
+
+	list = new_list;
+}
+
+let obj = list.reduce((obj, item) => {
+	obj[item.name] = (obj[item.name] || 0) + 1;
+	return obj;
+}, {});
+
+let things = Object.entries(obj);
+things.sort((a, b) => b[1] - a[1]);
+
+let min = things[0];
+let max = things[things.length - 1];
+
+console.log('min', min);
+console.log('max', max);
