f3f2d01a5cf5486f2d2a20c86dff5925c067812c
romellem <romellem.git@gmail.com>
2021-12-13 22:54:06 -0600

    Inits day 14

---
+++ 2021/14/input.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
+const input = fs
+	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\n')
+	.map((line) => {
+		return line.split('');
+	});
+
+module.exports = {
+	input,
+};
