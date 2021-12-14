cab2a87d3d20044dbc14ced9e7a8d07c95b6f5e2
romellem <romellem.git@gmail.com>
2021-12-13 23:02:34 -0600

    Parses input

--- 2021/14/input.js
+++ 2021/14/input.js
@@ -1,15 +1,19 @@
 const path = require('path');
 const fs = require('fs');
 
-const input = fs
+const [poly, raw_rules] = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
-	.split('\n')
-	.map((line) => {
-		return line.split('');
-	});
+	.split('\n\n');
+
+const rules = raw_rules.split('\n').map((rule) => {
+	// FC -> F
+	return ([from, to] = rule.split(' -> ').map((s) => s.trim()));
+	return { from, to };
+});
 
 module.exports = {
-	input,
+	poly,
+	rules,
 };
