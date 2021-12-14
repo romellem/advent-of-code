950188122a3714cdb59c7abd61abe03786231995
romellem <romellem.git@gmail.com>
2021-12-13 23:24:33 -0600

    Doesn't work

--- 2021/14/part-two.js
+++ 2021/14/part-two.js
@@ -17,8 +17,8 @@ class Element {
 
 let list = poly.split('').map((v) => new Element(v));
 
+let count = {};
 for (let i = 0; i < 10; i++) {
-	console.log(list.join(''));
 	for (let j = 0; j < list.length - 1; j++) {
 		let a = list[j];
 		let b = list[j + 1];
@@ -33,27 +33,41 @@ for (let i = 0; i < 10; i++) {
 	let new_list = [];
 	for (let j = 0; j < list.length; j++) {
 		let a = list[j];
-		new_list.push(a);
 
 		if (a.next) {
+			new_list.push(a);
 			new_list.push(a.next);
 			a.reset();
+		} else {
+			count[a.name] = (count[a.name] || 0) + 1;
 		}
 	}
 
 	list = new_list;
 }
 
+console.log(count);
+// process.exit(0);
+
 let obj = list.reduce((obj, item) => {
 	obj[item.name] = (obj[item.name] || 0) + 1;
 	return obj;
 }, {});
 
+console.log(obj);
+process.exit(0);
+
+for (let [name, x] of Object.entries(count)) {
+	obj[name] = (obj[name] || 0) + x;
+}
+
 let things = Object.entries(obj);
-things.sort((a, b) => b[1] - a[1]);
+things.sort((a, b) => a[1] - b[1]);
 
 let min = things[0];
 let max = things[things.length - 1];
 
 console.log('min', min);
 console.log('max', max);
+
+console.log(max[1] - min[1]);
