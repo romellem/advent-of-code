79365e0a4b4c2c1ab12bdfe43c39e7d96d52a085
romellem <romellem.git@gmail.com>
2021-12-16 23:01:02 -0600

    Saves, wrong answer??

--- 2021/17/part-one.js
+++ 2021/17/part-one.js
@@ -1,1 +1,87 @@
 const { input } = require('./input');
+const { InfiniteGrid } = require('./infinite-grid');
+
+// 1378, too low
+function findHighest(input) {
+	// math by paper
+	const INITIAL_XV = 11;
+	// const INITIAL_XV = 6;
+
+	const lowest = Math.min(...input.y);
+
+	let initial_yv = 0;
+	let yvs_that_work = [];
+
+	let failures_in_a_row = 0;
+
+	while (yvs_that_work.length === 0 || failures_in_a_row < 5) {
+		let worked_this_time = false;
+		// console.log(yvs_that_work);
+		let xv = INITIAL_XV;
+		let yv = initial_yv;
+
+		let x = 0;
+		let y = 0;
+
+		while (y > lowest) {
+			x += xv;
+			y += yv;
+
+			xv += xv === 0 ? 0 : xv > 0 ? -1 : 1;
+			yv--;
+
+			// console.log(
+			// 	`[${x}, ${y}], moving (${xv > -1 ? '+' : ''}${xv}, ${yv > -1 ? '+' : ''}${yv})`
+			// );
+
+			if (x >= input.x[0] && x <= input.x[1] && y >= input.y[0] && y <= input.y[1]) {
+				// console.log(`[${x}, ${y}] is in the target area`);
+				yvs_that_work.push(initial_yv);
+				failures_in_a_row = 0;
+				worked_this_time = true;
+			}
+		}
+
+		if (!worked_this_time) {
+			failures_in_a_row++;
+		}
+
+		initial_yv++;
+	}
+
+	const highest_position = Math.max(...yvs_that_work);
+
+	// Simulate the movement
+	let xv = INITIAL_XV;
+	let yv = highest_position;
+
+	let x = 0;
+	let y = 0;
+
+	let max_y = 0;
+
+	while (y > lowest) {
+		x += xv;
+		y += yv;
+
+		if (y > max_y) {
+			max_y = y;
+		}
+
+		xv += xv === 0 ? 0 : xv > 0 ? -1 : 1;
+		yv--;
+
+		// console.log(
+		// 	`[${x}, ${y}], moving (${xv > -1 ? '+' : ''}${xv}, ${yv > -1 ? '+' : ''}${yv})`
+		// );
+
+		if (x >= input.x[0] && x <= input.x[1] && y >= input.y[0] && y <= input.y[1]) {
+			break;
+		}
+	}
+
+	console.log(max_y);
+}
+
+findHighest(input);
+// findHighest({ x: [20, 30], y: [-10, -5] });
