bf41130b22106f285a129082a6906af7c8eaffac
romellem <romellem.git@gmail.com>
2021-12-16 18:53:08 -0600

    Saves again, I'm going nowhere

--- 2021/16/bits.js
+++ 2021/16/bits.js
@@ -6,6 +6,64 @@ const HEX_TO_DEC = '0123456789ABCDEF'
 	.split('')
 	.reduce((obj, char) => ((obj[char] = parseInt(char, 16)), obj), {});
 
+function parseHexAs4Bits(input_str) {
+	// Sizing the buffer is an optimizaiton, BitOutputStream will resize if it needs to
+	const out = new BitOutputStream(Math.ceil(input_str.length / 2));
+	for (let char of input_str) {
+		out.write(HEX_TO_DEC[char], 4);
+	}
+
+	return out;
+}
+
+function parseOutPackets(stream, packets = [], operator_length = { type, value }) {
+	while (stream.position < stream.length) {
+		/**
+		 * The first three bits encode the packet version,
+		 * and the next three bits encode the packet type ID.
+		 */
+		const version = stream.read(3);
+		const type = stream.read(3);
+		let value_stream;
+		if (type === LITERAL) {
+			value_stream = new BitOutputStream();
+			let should_continue;
+			do {
+				should_continue = stream.readBit();
+				value_stream.write(stream.read(4), 4);
+			} while (should_continue);
+
+			const value_bits = [...value_stream.reader()].join('');
+			const value = parseInt(value_bits, 2);
+
+			// Flush any padded zeros
+			if (stream.position % 4) {
+				const bits_left = 4 - (stream.position % 4);
+				stream.read(bits_left);
+			}
+
+			packets.push(new Literal(version, type, value));
+		} else {
+			/**
+			 * Operator packet
+			 *
+			 * An operator packet can use one of two modes indicated by
+			 * the bit immediately after the packet header; this is called the _length type ID_.
+			 * - If the length type ID is 0, then the next 15 bits are a
+			 *   number that represents the total length in bits of the sub-packets
+			 *   contained by this packet.
+			 * - If the length type ID is 1, then the next 11 bits are a
+			 *   number that represents the number of sub-packets immediately
+			 *   contained by this packet.
+			 */
+			operator_length.type = stream.readBit();
+			operator_length.value = length_type === 0 ? stream.read(15) : stream.read(11);
+		}
+	}
+
+	return packets;
+}
+
 export class Bits {
 	constructor(input) {
 		/** @type {BitOutputStream} */
@@ -87,12 +145,22 @@ export class Bits {
 	}
 }
 
-export class Packet {
+class Packet {
 	constructor(version, type, value) {
 		this.version = version;
 		this.type = type;
-		this.value = value;
+	}
+}
 
-		this.subpackets = [];
+class Literal extends Packet {
+	constructor(version, type, value) {
+		super(version, type);
+		this.value = value;
+	}
+}
+
+class Operator extends Packet {
+	constructor(version, type) {
+		super(version, type);
 	}
 }
