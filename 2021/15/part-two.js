df0ecadfe96a479e688edef16505846f549ff1c3
romellem <romellem.git@gmail.com>
2021-12-17 12:25:07 -0600

    Slight cleanup in building up large grids
    I don't need to do as much work, although admitedly this doesn't help
    the runtime that much. The pathfinding is the most expensive part.

--- 2021/15/part-two.js
+++ 2021/15/part-two.js
@@ -1,7 +1,5 @@
 const { input } = require('./input');
 const { InfiniteGrid } = require('./infinite-grid');
-const jsnx = require('jsnetworkx');
-const { concat } = require('lodash');
 
 let grid = new InfiniteGrid({
 	load: input,
@@ -16,7 +14,7 @@ function increaseGrid(grid) {
 			new_vale = 1;
 		}
 
-		new_grid.grid.set(id, new_vale);
+		new_grid.set(...InfiniteGrid.toCoords(id), new_vale);
 	}
 
 	return new_grid;
@@ -38,39 +36,45 @@ function concatGrids(...grids) {
 	return rows.join('\n');
 }
 
+/**
+ * If I have a grid `a`, and if I "increase that grid by 1" gives me `b`, then `c`, etc.,
+ * then expanding a grid to be 3x3 gives me a total grid that looks like
+ *
+ *     a b c
+ *     b c d
+ *     c d e
+ *
+ * You can see that grids `b` are copied "diagonally," and similarly for `c` and `d`.
+ *
+ * So I don't have to make unique grids for each cell. In this 3x3 expanded grid, I only have
+ * 5 unique grids among 9 cells.
+ */
 function buildGridOutDiagonally(grid, copies = 5) {
-	let rows = [];
-	for (let y = 0; y < copies; y++) {
-		let first_col = rows.length ? rows[rows.length - 1][1] : grid;
-		let row_of_grids = [first_col];
+	let row_of_grids = [grid];
+	// The first row requires the most amount of work, because we only get to reuse the first cell
+	for (let x = 1; x < copies; x++) {
+		let last_col = row_of_grids[row_of_grids.length - 1];
+		row_of_grids.push(increaseGrid(last_col));
+	}
 
-		// a b c d e
-		// b c d e f
-		// c d e f g
-		// d e f g h
-		// e f g h i
-		for (let x = 1; x < copies; x++) {
-			let current_col = row_of_grids[row_of_grids.length - 1];
-			row_of_grids.push(increaseGrid(current_col));
-		}
+	let rows = [row_of_grids];
+
+	// After that, we can reuse all except 1 of the previously generated grids in the next row
+	for (let y = 1; y < copies; y++) {
+		let last_row = rows[rows.length - 1];
+		let last_col = last_row[last_row.length - 1];
+
+		// Copy all grids except the first one
+		row_of_grids = last_row.slice(1);
+		row_of_grids.push(increaseGrid(last_col));
 
 		rows.push(row_of_grids);
 	}
 
-	console.log(
-		'new subgrids',
-		rows.map((v) => v.length).reduce((a, b) => a + b)
-	);
-	process.exit(1);
-
-	// [[a, b, c, d, e]];
-
-	const rows_strs = rows.map((row) => concatGrids(...row));
-	console.log();
+	// Now concatenate all rows to create giant wide strings, then join on '\n' to create a GIANT grid of numbers.
 	const giant_grid_input = rows.map((row) => concatGrids(...row)).join('\n');
-	console.log(giant_grid_input);
-	process.exit(1);
 
+	// Load this new input to create our new larger grid to pathfind on
 	return new InfiniteGrid({
 		load: giant_grid_input,
 		parseAs: Number,
@@ -78,6 +82,8 @@ function buildGridOutDiagonally(grid, copies = 5) {
 }
 
 const giant_grid = buildGridOutDiagonally(grid, 5);
+
+// Same steps as before
 const path = giant_grid.getShortestWeightedPath(0, 0, giant_grid.max_x, giant_grid.max_y, {
 	include_from: false,
 });
