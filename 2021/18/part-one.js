714dfa5afbfcd04a117af606e89434d80b0b46f1
romellem <romellem.git@gmail.com>
2021-12-18 15:13:44 -0600

    Adds tests

--- 2021/18/part-one.js
+++ 2021/18/part-one.js
@@ -1,17 +1,41 @@
-const { input } = require('./input');
+const { input, splitLineWithNumbers } = require('./input');
 const { add, reduce, magnitude } = require('./snailfish');
+const assert = require('assert');
 
-let accumulator = input.shift();
-while (input.length > 0) {
-	let next_pair = input.shift();
+function partOne() {
+	let accumulator = input.shift();
+	while (input.length > 0) {
+		let next_pair = input.shift();
 
-	accumulator = add(accumulator, next_pair);
-	reduce(accumulator);
+		accumulator = add(accumulator, next_pair);
+		reduce(accumulator);
+	}
+
+	const mag = magnitude(splitLineWithNumbers(accumulator));
+
+	console.log(mag);
 }
 
-const mag = magnitude(accumulator);
+function testMagnitude() {
+	const tests = [
+		['[[1,2],[[3,4],5]]', 143],
+		['[[[[0,7],4],[[7,8],[6,0]]],[8,1]]', 1384],
+		['[[[[1,1],[2,2]],[3,3]],[4,4]]', 445],
+		['[[[[3,0],[5,3]],[4,4]],[5,5]]', 791],
+		['[[[[5,0],[7,4]],[5,5]],[6,6]]', 1137],
+		['[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]', 3488],
+	];
 
-console.log(mag);
+	for (let [str, expected] of tests) {
+		const mag = magnitude(splitLineWithNumbers(str));
+
+		assert.strictEqual(mag, expected);
+	}
+}
+
+testMagnitude();
+
+// partOne();
 
 // 3900 too low
 // 3990 too low
