3d65f030178c41759c7edae2f575789575b1836a
romellem <romellem.git@gmail.com>
2021-12-18 15:12:02 -0600

    Exports split helper

--- 2021/18/input.js
+++ 2021/18/input.js
@@ -1,18 +1,20 @@
 const path = require('path');
 const fs = require('fs');
 
+const splitLineWithNumbers = (line) =>
+	line.split('').map((v) =>
+		// Input only has single digit numbers
+		/\d/.test(v) ? parseInt(v, 10) : v
+	);
+
 const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
 	.split('\n')
-	.map((line) =>
-		line.split('').map((v) =>
-			// Input only has single digit numbers
-			/\d/.test(v) ? parseInt(v, 10) : v
-		)
-	);
+	.map((line) => splitLineWithNumbers(line));
 
 module.exports = {
 	input,
+	splitLineWithNumbers,
 };
