7c272cdde639af7a553c32e0745c991bf368bf97
romellem <romellem.git@gmail.com>
2021-12-18 14:01:09 -0600

    First attempt at part one, doesn't work

--- 2021/18/part-one.js
+++ 2021/18/part-one.js
@@ -1,1 +1,12 @@
 const { input } = require('./input');
+const { add, reduce } = require('./snailfish');
+
+let accumulator = input.shift();
+while (input.length > 0) {
+	let next_pair = input.shift();
+
+	accumulator = add(accumulator, next_pair);
+	reduce(accumulator);
+}
+
+console.log(accumulator.join(''));
