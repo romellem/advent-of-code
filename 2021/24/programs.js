acb3feac102646b584aa7b4489238da38042d6c5
romellem <romellem.git@gmail.com>
2021-12-24 13:24:07 -0600

    Saves, getting closer?

--- 2021/24/programs.js
+++ 2021/24/programs.js
@@ -3,6 +3,8 @@
 // 2: z = [(i0 + 5) * 26 + (i1 + 9)] * 26 + (i2 + 4)
 // 3: x = i2 == 9 && i3 == 1 ? 0 : 1, z = z, otherwise z = z * 26 + (i3 + 4)
 // 4: z = [[(i0 + 5) * 26 + (i1 + 9)] * 26 + (i2 + 4)] * 26 + (i4 + 10)
+// 5: z = [[(i0 + 5) * 26 + (i1 + 9)] * 26 + (i2 + 4)] * 26 + (i5 + 14)
+// 6:
 
 // 9:14
 function part0(input) {
@@ -36,7 +38,7 @@ function part3(input) {
 	z = z + y;
 }
 
-// 9:(6*26)+19=175
+// 9:14
 function part4(input) {
 	x = 1;
 	y = input + 10; // Y = 11 - 19
@@ -44,31 +46,27 @@ function part4(input) {
 }
 
 function part5(input) {
-	w = input;
-
-	z = Math.trunc(z / 26); // 6 - 13 (since z was `(6-13)*26 + (11-19)`, it effectively rounds off the (11-19) addition)
+	z = Math.trunc(z / 26);
 	x = (z % 26) - 13; // (6 - 13) - 13 = -7 - 0
-	x = x === w ? 0 : 1; // Can never be true, x = 1
+	x = x === input ? 0 : 1; // Can never be true, x = 1
 
-	z = z * (25 * x + 1); // (6-13) * 26
-	y = (w + 14) * x; // 15-23
+	z = z * (25 * x + 1); //
+	y = (input + 14) * x; // 15-23
 
-	z = z + y; // [(6-13) * 26] + (15-23)
+	z = z + y; // [n * 26] + (15-23)
 }
 
 function part6(input) {
-	w = input;
-
 	x = (z % 26) - 9; // 6 - 14
 
 	z = Math.trunc(z / 26); // 6 - 13
 
-	x = x === w ? 0 : 1; // Could be true if input is 6-9
+	x = x === input ? 0 : 1; // Could be true if input is 6-9
 
 	y = 25 * x + 1; // 1 or 26
 	z = z * y; // (6-13) * (1 or 26)
 
-	y = (w + 14) * x; // (15-19) or 0
+	y = (input + 14) * x; // (15-19) or 0
 	z = z + y; // [(6-13) * (1 or 26)] + (15-19) or 0
 }
 
