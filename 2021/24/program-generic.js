54f562a193cdbbac838ace7b2ee4fec756653f19
romellem <romellem.git@gmail.com>
2021-12-28 17:07:23 -0800

    Got an answer! Not correct though

--- 2021/24/program-generic.js
+++ 2021/24/program-generic.js
@@ -90,22 +90,36 @@ function tick3(input, x_inc, y_inc) {
 
 let w = (x = y = z = 0);
 
+14;
+14, 18;
+14, 18, 13;
+14, 18;
+14, 18, 15;
+14, 18;
+14;
+18;
+11;
+11, 19;
+11;
+
+// 99915292496939 gives me 0 at the end, but it says it is _too low_
+
 // prettier-ignore
 for (let args of [
-	/*  1 */ { truncate_z: false, x_inc:  10, y_inc:  5, input: 9 }, // (prev_input + prev_y_inc) + x_inc = someValidInput?
-	/*  2 */ { truncate_z: false, x_inc:  13, y_inc:  9, input: 9 },
-	/*  3 */ { truncate_z: false, x_inc:  12, y_inc:  4, input: 9 },
-	/*  4 */ { truncate_z: true,  x_inc: -12, y_inc:  4, input: 1 }, // (9 + 4) - 12 = 1
-	/*  5 */ { truncate_z: false, x_inc:  11, y_inc: 10, input: 5 }, // 5
-	/*  6 */ { truncate_z: true,  x_inc: -13, y_inc: 14, input: 2 }, // (5 + 10) - 13 = 2
-	/*  7 */ { truncate_z: true,  x_inc:  -9, y_inc: 14, input: 7 }, // (2 + 14) - 9 = 7
-	/*  8 */ { truncate_z: true,  x_inc: -12, y_inc: 12, input: 9 }, // (7 + 14) - 12 = 9
-	/*  9 */ { truncate_z: false, x_inc:  14, y_inc: 14, input: 4 }, // 4
-	/* 10 */ { truncate_z: true,  x_inc:  -9, y_inc: 14, input: 9 }, // (4 + 14) - 9 = 9
-	/* 11 */ { truncate_z: false, x_inc:  15, y_inc:  5, input: 9 },
-	/* 12 */ { truncate_z: false, x_inc:  11, y_inc: 10, input: 9 },
-	/* 13 */ { truncate_z: true,  x_inc: -16, y_inc:  8, input: 3 }, // (9 + 10) - 16 = 3
-	/* 14 */ { truncate_z: true,  x_inc:  -2, y_inc: 15, input: 9 }, // (3 + 8) - 2 = 9
+	/*  1 */ { truncate_z: false, x_inc:  10, y_inc:  5, input: 9 }, // push 5 + 9 = 14
+	/*  2 */ { truncate_z: false, x_inc:  13, y_inc:  9, input: 9 }, // push 9 + 9 = 18
+	/*  3 */ { truncate_z: false, x_inc:  12, y_inc:  4, input: 9 }, // push 4 + 9 = 13
+	/*  4 */ { truncate_z: true,  x_inc: -12, y_inc:  4, input: 1 }, // pop 13 - 12 = 1
+	/*  5 */ { truncate_z: false, x_inc:  11, y_inc: 10, input: 5 }, // push 10 + 5 = 15
+	/*  6 */ { truncate_z: true,  x_inc: -13, y_inc: 14, input: 2 }, // pop 15 - 13 = 2
+	/*  7 */ { truncate_z: true,  x_inc:  -9, y_inc: 14, input: 9 }, // pop 18 - 9 = 9
+	/*  8 */ { truncate_z: true,  x_inc: -12, y_inc: 12, input: 2 }, // pop 14 - 12 = 2
+	/*  9 */ { truncate_z: false, x_inc:  14, y_inc: 14, input: 4 }, // push 14 + 4 = 18
+	/* 10 */ { truncate_z: true,  x_inc:  -9, y_inc: 14, input: 9 }, // pop 18 - 9
+	/* 11 */ { truncate_z: false, x_inc:  15, y_inc:  5, input: 6 }, // push 5 + 9 = 11
+	/* 12 */ { truncate_z: false, x_inc:  11, y_inc: 10, input: 9 }, // push 10 + 9 = 19
+	/* 13 */ { truncate_z: true,  x_inc: -16, y_inc:  8, input: 3 }, // pop 19 - 16
+	/* 14 */ { truncate_z: true,  x_inc:  -2, y_inc: 15, input: 9 }, // pop 11 - 2
 ]) {
 	tick2(args);
 }
