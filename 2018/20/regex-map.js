90f537434c7a91a6f804bfd09fa7b53cdaa91916
romellem <romellem.git@gmail.com>
2022-05-06 18:10:05 -0400

    Saves progress, I can simplify greatly
    So if I have ^N(E|W)S$, that is a map that looks like
    
        #######
        #.|.|.#
        #-#-#-#
        #.#X#.#
        #######
    
    Crucially, after the `(E|W), I have now *two* origins I can go South
    from, which complicates things. If this input, you only ever have to
    track a single origin. When I come to an open paren, I push on the
    origin, upon a closing, I pop it off. No need to track different
    origins. That simplifies things a good amount.

--- 2018/20/regex-map.js
+++ 2018/20/regex-map.js
@@ -1,9 +1,17 @@
-class Node {
-	constructor() {
+class RegexNode {
+	constructor(x, y) {
+		this.id = `${x},${y}`;
+		this.coord = { x, y };
+
 		this.N = undefined;
 		this.S = undefined;
 		this.E = undefined;
 		this.W = undefined;
+	}
+
+	idToCoord() {
+		let [x, y] = this.id.split(',');
+		return { x: Number(x), y: Number(y) };
 	}
 
 	connect(node, fromSelfToDir) {
@@ -31,8 +39,39 @@ class Node {
 	}
 }
 
-class Map {
+class RegexMap {
 	constructor() {
-		this.center = new Node();
+		this.center = new RegexNode(0, 0);
+	}
+
+	connect(from, to, dir) {
+		from.connect(to, dir);
+	}
+
+	build(path) {
+		let origins = [{ x: 0, y: 0 }];
+		let stack = [];
+		let nodes = new Map();
+		nodes.set(this.center.id, this.center);
+
+		for (let char of path) {
+			if (char === '^' || char === '$') {
+				continue;
+			}
+
+			for (let origin of origins) {
+				if (char === '(') {
+					stack.push(atNode);
+					origins.push(atNode);
+					break;
+				} else if (char === ')') {
+					atNode = stack.pop();
+				} else if (char === '|') {
+					atNode = stack[stack.length - 1];
+				} else {
+					// Move in a direction
+				}
+			}
+		}
 	}
 }
