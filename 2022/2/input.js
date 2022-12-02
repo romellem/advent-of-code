4ecb22785fe366887523efdc28a99c01ad196907
romellem <romellem.git@gmail.com>
2022-12-01 23:30:55 -0600

    Working part one

--- 2022/2/input.js
+++ 2022/2/input.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const fs = require('fs');
 
+const SHAPES = {
+	A: 1,
+	X: 1,
+	B: 2,
+	Y: 2,
+	C: 3,
+	Z: 3,
+};
+
 const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
 	.split('\n')
-	.map((v) => +v);
+	.map((v) => v.split(' ').map((v) => SHAPES[v]));
 
 module.exports = {
 	input,
+	SHAPES,
 };
--- 2022/2/part-one.js
+++ 2022/2/part-one.js
@@ -1,8 +1,14 @@
 const { input } = require('./input');
 
-let acc = [];
-for (let val of input) {
-	//
-}
+const rounds = input.map(([left, right]) => {
+	const diff = Math.abs(left - right);
+	if (left === right) {
+		return right + 3;
+	} else if ((diff === 1 && right > left) || (right === 1 && left === 3)) {
+		return right + 6;
+	} else {
+		return right;
+	}
+});
 
-console.log(acc);
+console.log(rounds.reduce((a, b) => a + b, 0));
