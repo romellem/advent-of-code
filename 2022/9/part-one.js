c4913c5ad8be936b227981627c84aa3a50497416
romellem <romellem.git@gmail.com>
2022-12-08 23:31:08 -0600

    Working!
    Wasn't account for overlaps

--- 2022/9/part-one.js
+++ 2022/9/part-one.js
@@ -7,19 +7,27 @@ grid.set(0, 0, 's');
 let head = [0, 0];
 let tail = [0, 0];
 
-let tail_visted = new Set(InfiniteGrid.toId(...tail));
+let tail_visted = new Set([InfiniteGrid.toId(...tail)]);
 
 for (let { dir, steps } of input) {
 	for (let i = 0; i < steps; i++) {
 		let previousHead = head;
 		head = InfiniteGrid.moveInDirection(...head, dir);
 		grid.set(...head, 'H');
+		grid.set(...previousHead, '.');
+		grid.set(...tail, '.');
 		let neighbors = grid.neighbors(...head, true);
-		if (!neighbors.has(InfiniteGrid.toId(...tail))) {
+		let neighbor_ids = new Set(
+			[...neighbors.values()].map((v) => v.id).concat(InfiniteGrid.toId(...head))
+		);
+		if (!neighbor_ids.has(InfiniteGrid.toId(...tail))) {
 			// Move tail
 			tail = previousHead;
-			tail_visted.add(InfiniteGrid.toId(tail));
+			tail_visted.add(InfiniteGrid.toId(...tail));
 		}
+
+		grid.set(...tail, 'T');
+		grid.set(0, 0, 's');
 	}
 }
 
