d45e6c0522d5b2b8be8fc24803e0102334ab9207
romellem <romellem.git@gmail.com>
2022-12-08 21:25:19 -0600

    Uses same filter idea from part two

--- 2022/7/part-one.js
+++ 2022/7/part-one.js
@@ -2,14 +2,13 @@ const { input } = require('./input');
 const { Filesystem } = require('./filesystem');
 
 const drive = new Filesystem(input);
+const dirs = [...drive].filter((v) => v.isDir);
 
 let smallDirsSum = 0;
-for (let item of drive) {
-	if (item.isDir) {
-		let size = item.size();
-		if (size <= 100000) {
-			smallDirsSum += size;
-		}
+for (let item of dirs) {
+	let size = item.size();
+	if (size <= 100000) {
+		smallDirsSum += size;
 	}
 }
 
