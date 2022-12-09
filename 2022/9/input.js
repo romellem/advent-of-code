e12aa29b5e51255de08cad3aecefb526761b6680
romellem <romellem.git@gmail.com>
2022-12-08 23:13:20 -0600

    Initial work, wrong answer

--- 2022/9/input.js
+++ 2022/9/input.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const fs = require('fs');
 
+const dirToCompass = {
+	U: 'N',
+	D: 'S',
+	L: 'W',
+	R: 'E',
+};
+
 const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
 	.split('\n')
 	.map((v) => {
-		return v;
+		let [dir, steps] = v.split(' ');
+		dir = dirToCompass[dir];
+		steps = parseInt(steps, 10);
+		return { dir, steps };
 	});
 
 module.exports = {
--- 2022/9/part-one.js
+++ 2022/9/part-one.js
@@ -1,4 +1,26 @@
 const { input } = require('./input');
-const G = require('generatorics');
+const { InfiniteGrid } = require('./infinite-grid');
 
-console.log();
+let grid = new InfiniteGrid({ defaultFactory: () => `.` });
+grid.set(0, 0, 's');
+
+let head = [0, 0];
+let tail = [0, 0];
+
+let tail_visted = new Set(InfiniteGrid.toId(...tail));
+
+for (let { dir, steps } of input) {
+	for (let i = 0; i < steps; i++) {
+		let previousHead = head;
+		head = InfiniteGrid.moveInDirection(...head, dir);
+		grid.set(...head, 'H');
+		let neighbors = grid.neighbors(...head, true);
+		if (!neighbors.has(InfiniteGrid.toId(...tail))) {
+			// Move tail
+			tail = previousHead;
+			tail_visted.add(InfiniteGrid.toId(tail));
+		}
+	}
+}
+
+console.log(tail_visted.size);
