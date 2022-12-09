1b8ca6baa13f6f2992031bbd668f875ac1b77793
romellem <romellem.git@gmail.com>
2022-12-08 18:55:41 -0600

    Attempt, to high

--- 2022/8/part-two.js
+++ 2022/8/part-two.js
@@ -1,5 +1,6 @@
 const { input } = require('./input');
 const { InfiniteGrid } = require('./infinite-grid');
+const { max } = require('lodash');
 
 const grid = new InfiniteGrid({
 	load: input,
@@ -7,6 +8,20 @@ const grid = new InfiniteGrid({
 		return { value: parseInt(cell, 10), visible: undefined };
 	},
 });
+
+function countTrees(slice, from) {
+	let count = 0;
+	for (let cell of slice) {
+		if (cell.value >= from.value) {
+			break;
+		}
+		count++;
+	}
+
+	return count;
+}
+
+let max_score = -1;
 
 // Naive O(n^2) way
 for (let [cell_id, cell] of grid) {
@@ -22,16 +37,19 @@ for (let [cell_id, cell] of grid) {
 	let top = col.slice(0, colIndex);
 	let down = col.slice(colIndex + 1);
 
-	if (
-		left.every((v) => v.value < cell.value) ||
-		right.every((v) => v.value < cell.value) ||
-		top.every((v) => v.value < cell.value) ||
-		down.every((v) => v.value < cell.value)
-	) {
-		cell.visible = true;
-	} else {
-		cell.visible = false;
-	}
+	// Iterate from inside out. `right` & `down` already are ordered this way
+	left.reverse();
+	top.reverse();
+
+	let scenic_score = [
+		countTrees(left, cell.value),
+		countTrees(right, cell.value),
+		countTrees(top, cell.value),
+		countTrees(down, cell.value),
+	].reduce((a, b) => a * b, 1);
+
+	cell.score = scenic_score;
+	max_score = Math.max(max_score, scenic_score);
 }
 
-console.log(count); // 1025 (too low)
+console.log(max_score); // 5764801 (too high)
