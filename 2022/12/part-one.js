daf082dd69e3324ceb971911e1af1759ce25d4b2
romellem <romellem.git@gmail.com>
2022-12-11 23:39:01 -0600

    This should work...

--- 2022/12/part-one.js
+++ 2022/12/part-one.js
@@ -1,1 +1,19 @@
 const { input } = require('./input');
+const { InfiniteGrid } = require('./infinite-grid');
+
+const S = 'a'.charCodeAt(0) - 1;
+const E = 'z'.charCodeAt(0) + 1;
+const grid = new InfiniteGrid({
+	load: input,
+	parseAs: (char) => (char === 'S' ? S : char === 'E' ? E : char.charCodeAt(0)),
+});
+
+let [start] = grid.findAll(S);
+let [end] = grid.findAll(E);
+
+let [start_x, start_y] = start[1];
+let [end_x, end_y] = end[1];
+
+let a = grid.getShortestElevationPath(start_x, start_y, end_x, end_y);
+
+console.log(a.length - 1);
