7538ba79f8c789ffd194d740c906beab86d2fc3a
romellem <romellem.git@gmail.com>
2022-12-13 17:16:49 -0600

    Recursion working, sums is wrong
    Misread what I'm supposed to add up

--- 2022/13/part-one.js
+++ 2022/13/part-one.js
@@ -1,32 +1,54 @@
 const { input } = require('./input');
 
-function compare(left, right, i = 0) {
-	while (true) {
+function compare(left, right) {
+	for (let i = 0; i < Math.max(left.length, right.length); i++) {
 		let li = left[i];
 		let ri = right[i];
-		if (li === undefined || ri === undefined) {
+
+		if (li === undefined) {
+			return true;
+		} else if (ri === undefined) {
 			return false;
 		}
 
-		if (typeof li === 'number' && typeof ri !== 'number') {
-			return compare([li], ri, i);
-		} else if (typeof ri === 'number' && typeof li !== 'number') {
-			return compare(li, [ri], i);
-		}
-
-		if (typeof li !== 'number' && typeof ri !== 'number') {
-			return compare(li, ri, 0);
-		}
-
 		if (typeof li === 'number' && typeof ri === 'number') {
+			if (li < ri) {
+				return true;
+			} else if (ri > li) {
+				return false;
+			} else {
+				// Same num, continue checking next val
+				continue;
+			}
+		}
+
+		if (Array.isArray(li) && Array.isArray(ri)) {
+			let substep = compare(li, ri);
+			if (substep !== null) {
+				return substep;
+			}
+		}
+
+		if (Array.isArray(li) && !Array.isArray(ri)) {
+			return compare(li, [ri]);
+		}
+
+		if (!Array.isArray(li) && Array.isArray(ri)) {
+			return compare([li], ri);
 		}
 	}
+
+	// This (sub)list was neither right nor wrong
+	return null;
 }
-// for (let [left, right] of input) {
-// 	let l_item, r_item;
-// 	let
-// 	do {
 
-// 	} while (l_item !== undefined || r_item !== undefined)
-
-// }
+let sums = 0;
+for (let [left, right] of input) {
+	let result = compare(left, right);
+	if (result === true) {
+		sums += [left, right].flat(Infinity).reduce((a, b) => a + b, 0);
+	} else if (result === null) {
+		console.log('oops', left, right);
+	}
+}
+console.log(sums);
