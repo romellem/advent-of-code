e161cb3115ddc16dc9da9da089cfa3333f99eecd
romellem <romellem.git@gmail.com>
2022-12-13 22:40:02 -0600

    Debugging

--- 2022/13/part-two.js
+++ 2022/13/part-two.js
@@ -3,6 +3,9 @@ const { input } = require('./input');
 const divider2 = [[2]];
 const divider6 = [[6]];
 
+// Turn this on to see logs
+const debug = false;
+
 // prettier-ignore
 const input_all = [
 	...input.flat(),
@@ -10,14 +13,23 @@ const input_all = [
 	divider6,
 ];
 
-compare([[1], [2, 3, 4]], [1, 1, 5, 1, 1]);
+function compare(left, right, depth = 0, debug = false) {
+	if (depth === 0 && debug) {
+		console.log('========');
+	}
 
-function compare(left, right, depth = 0) {
 	for (let i = 0; i < Math.max(left.length, right.length); i++) {
 		let li = left[i];
 		let ri = right[i];
 
-		// console.log('Compare'.padStart(8 + depth), s(li), 'vs', s(ri));
+		if (debug) {
+			console.log(
+				'Compare'.padStart(7 + depth * 2),
+				JSON.stringify(li),
+				'vs',
+				JSON.stringify(ri)
+			);
+		}
 
 		if (li === undefined) {
 			return true;
@@ -37,18 +49,34 @@ function compare(left, right, depth = 0) {
 		}
 
 		if (Array.isArray(li) && Array.isArray(ri)) {
-			let substep = compare(li, ri, depth + 1);
+			let substep = compare(li, ri, depth + 1, debug);
 			if (substep !== null) {
 				return substep;
 			}
-		}
+		} else if (Array.isArray(li) && !Array.isArray(ri)) {
+			if (debug) {
+				console.log(
+					' '.repeat(depth * 2) +
+						`Mixed types; convert right to [${ri}] and retry comparison`
+				);
+			}
 
-		if (Array.isArray(li) && !Array.isArray(ri)) {
-			return compare(li, [ri], depth + 1);
-		}
+			let substep = compare(li, [ri], depth + 1, debug);
+			if (substep !== null) {
+				return substep;
+			}
+		} else if (!Array.isArray(li) && Array.isArray(ri)) {
+			if (debug) {
+				console.log(
+					' '.repeat(depth * 2) +
+						`Mixed types; convert left to [${li}] and retry comparison`
+				);
+			}
 
-		if (!Array.isArray(li) && Array.isArray(ri)) {
-			return compare([li], ri, depth + 1);
+			let substep = compare([li], ri, depth + 1, debug);
+			if (substep !== null) {
+				return substep;
+			}
 		}
 	}
 
@@ -57,18 +85,21 @@ function compare(left, right, depth = 0) {
 }
 
 input_all.sort((a, b) => {
-	const comparison = compare(a, b);
+	const comparison = compare(a, b, undefined, debug);
 	if (comparison === true) {
 		return -1;
 	} else if (comparison === false) {
 		return 1;
 	} else {
-		// console.log('Oops', a, b);
 		return 0;
 	}
 });
 
-// input_all.forEach((l) => console.log(JSON.stringify(l)));
+if (debug) {
+	console.log('Sorted list:');
+	input_all.forEach((l) => console.log(JSON.stringify(l)));
+}
+
 let a = input_all.indexOf(divider2);
 let b = input_all.indexOf(divider6);
 console.log((a + 1) * (b + 1));
