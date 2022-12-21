71a463487659c39d87b3e7069af89497721d0130
romellem <romellem.git@gmail.com>
2022-12-21 17:13:33 -0600

    Cleans up old comments

--- 2022/13/part-one.js
+++ 2022/13/part-one.js
@@ -1,13 +1,9 @@
 const { input } = require('./input');
 
-const s = (v) => JSON.stringify(v);
-
 function compare(left, right, depth = 0) {
 	for (let i = 0; i < Math.max(left.length, right.length); i++) {
 		let li = left[i];
 		let ri = right[i];
-
-		// console.log('Compare'.padStart(8 + depth), s(li), 'vs', s(ri));
 
 		if (li === undefined) {
 			return true;
@@ -49,14 +45,12 @@ function compare(left, right, depth = 0) {
 let correct = [];
 for (let i = 0; i < input.length; i++) {
 	let [left, right] = input[i];
-	// console.log(`\n== Pair ${i + 1} ==`);
 	let result = compare(left, right);
 	if (result === true) {
 		correct.push(i + 1);
 	} else if (result === null) {
-		console.log('oops', i, JSON.stringify(left), JSON.stringify(right));
-	} else {
-		// console.log('yeah');
+		throw new Error(`Failed to compare:\n${JSON.stringify(left)}\n${JSON.stringify(right)}`);
 	}
 }
+
 console.log(correct.reduce((a, b) => a + b, 0));
