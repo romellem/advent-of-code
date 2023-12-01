2ba1cc5030c7867ff205445d0e119885e90dcc85
romellem <romellem.git@gmail.com>
2023-11-30 23:23:25 -0600

    wtf

--- 2023/1/part-two.js
+++ 2023/1/part-two.js
@@ -16,15 +16,22 @@ function doThing(input) {
 	debugger;
 	let sum = 0;
 	for (let line of input) {
-		let indices = Object.keys(nums)
-			.map((n) => [n, line.indexOf(n)])
-			.filter((n) => n[1] !== -1)
-			.sort((a, b) => a[1] - b[1]);
-		for (let [word] of indices) {
-			line = line.replace(word, nums[word]);
-		}
+		let origline = line;
+		let indices = [];
+		do {
+			indices = Object.keys(nums)
+				.map((n) => [n, line.indexOf(n)])
+				.filter((n) => n[1] !== -1)
+				.sort((a, b) => a[1] - b[1]);
+			for (let [word] of indices) {
+				line = line.replace(word, nums[word]);
+			}
+		} while (indices.length > 0);
 		let nums2 = line.split('').filter((v) => /\d/.test(v));
 		const val = nums2[0] + nums2[nums2.length - 1];
+		console.log(origline, '===\n' + line, '=', nums2.join(''), '=', val);
+		console.log();
+
 		sum += +val;
 	}
 	return sum;
