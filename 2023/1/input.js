e0ffc0a855f2b9a7dfe70f11ad6d4a9079732f2e
romellem <romellem.git@gmail.com>
2023-11-30 23:11:53 -0600

    save, not right

--- 2023/1/input.js
+++ 2023/1/input.js
@@ -5,11 +5,7 @@ const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
-	.split('\n')
-	.map((v) => {
-		let a = v.split('').filter((v) => /\d/.test(v));
-		return a[0] + a[a.length - 1];
-	});
+	.split('\n');
 
 module.exports = {
 	input,
--- 2023/1/part-two.js
+++ 2023/1/part-two.js
@@ -1,8 +1,33 @@
 const { input } = require('./input');
 
+const nums = {
+	one: 1,
+	two: 2,
+	three: 3,
+	four: 4,
+	five: 5,
+	six: 6,
+	seven: 7,
+	eight: 8,
+	nine: 9,
+};
+
 function doThing(input) {
-	const val = input;
-	return val;
+	debugger;
+	let sum = 0;
+	for (let line of input) {
+		let indices = Object.keys(nums)
+			.map((n) => [n, line.indexOf(n)])
+			.filter((n) => n[1] !== -1)
+			.sort((a, b) => a[1] - b[1]);
+		for (let [word] of indices) {
+			line = line.replace(word, nums[word]);
+		}
+		let nums2 = line.split('').filter((v) => /\d/.test(v));
+		const val = nums2[0] + nums2[nums2.length - 1];
+		sum += +val;
+	}
+	return sum;
 }
 
 const val = doThing(input);
