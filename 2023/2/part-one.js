f0b24dd5412e61ed645fec5d9c37ca97a2cd8b95
romellem <romellem.git@gmail.com>
2023-12-01 23:10:16 -0600

    save, works

--- 2023/2/part-one.js
+++ 2023/2/part-one.js
@@ -1,9 +1,43 @@
 const { input } = require('./input');
 
-function doThing(input) {
-	return input;
+function parse(input) {
+	// Game 70: 1 red, 19 green; 4 blue, 6 green; 12 green, 2 red
+	return input.map((line) => {
+		let [, id, rest] = /Game (\d+): (.+)$/.exec(line);
+		let rounds = rest.split('; ');
+		let cubesInEachRound = rounds.map((round) =>
+			round.split(', ').map((pick) => {
+				return pick
+					.trim()
+					.split(' ')
+					.map((v, i) => (i === 0 ? +v : v));
+			})
+		);
+
+		/*
+			"cubesInEachRound": [
+				[
+					[8, "red"],
+					[4, "blue"],
+					[4, "green"]
+				],
+		*/
+		return {
+			id: +id,
+			cubesInEachRound,
+			maxCubes: cubesInEachRound.reduce(
+				(acc, round) => {
+					return round.reduce((acc2, [num, color]) => {
+						acc2[color] = Math.max(acc2[color], num);
+						return acc2;
+					}, acc);
+				},
+				{ blue: 0, red: 0, green: 0 }
+			),
+		};
+	});
 }
 
-const val = doThing(input);
+// parse(input)
 
-console.log(val);
+console.log(JSON.stringify(parse(input)));
