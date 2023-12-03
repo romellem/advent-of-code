5641859119aaa1219ad3cf239f259e41b040fe57
romellem <romellem.git@gmail.com>
2023-12-03 16:27:35 -0600

    Refactors part one and two

--- 2023/2/part-one.js
+++ 2023/2/part-one.js
@@ -1,36 +1,34 @@
 const { input } = require('./input');
 
 function parse(input) {
+	// @example
 	// Game 70: 1 red, 19 green; 4 blue, 6 green; 12 green, 2 red
 	return input.map((line) => {
-		let [, id, rest] = /Game (\d+): (.+)$/.exec(line);
-		let rounds = rest.split('; ');
-		let cubesInEachRound = rounds.map((round) =>
-			round.split(', ').map((pick) => {
-				return pick
-					.trim()
-					.split(' ')
-					.map((v, i) => (i === 0 ? +v : v));
-			})
-		);
+		const [, id, rest] = /Game (\d+): (.+)$/.exec(line);
 
-		/*
-			"cubesInEachRound": [
-				[
-					[8, "red"],
-					[4, "blue"],
-					[4, "green"]
-				],
-		*/
+		// Rounds don't matter, take a list of all groups of cubes picked
+		const picks = rest.split(/[;,] /);
+		/** @type {Array<[number, string]>} */
+		const cubesPicked = picks.map((pick) => {
+			let num, color;
+			try {
+				[, num, color] = /(\d+) (\w+)/.exec(pick);
+			} catch (e) {
+				console.log(e);
+				console.log({ pick, rest });
+				console.log({ num, color });
+				process.exit(1);
+			}
+
+			return [parseInt(num, 10), color];
+		});
+
 		return {
-			id: +id,
-			cubesInEachRound,
-			maxCubes: cubesInEachRound.reduce(
-				(acc, round) => {
-					return round.reduce((acc2, [num, color]) => {
-						acc2[color] = Math.max(acc2[color], num);
-						return acc2;
-					}, acc);
+			id: parseInt(id, 10),
+			maxCubes: cubesPicked.reduce(
+				(acc, [num, color]) => {
+					acc[color] = Math.max(acc[color], num);
+					return acc;
 				},
 				{ blue: 0, red: 0, green: 0 }
 			),
@@ -38,16 +36,13 @@ function parse(input) {
 	});
 }
 
-let val = parse(input)
-	.map((v, i) => {
-		const { maxCubes } = v;
-		if (maxCubes.red <= 12 && maxCubes.green <= 13 && maxCubes.blue <= 14) {
-			return [true, i + 1];
-		} else {
-			return [false, i + 1];
-		}
-	})
-	.filter((v) => v[0] === true)
-	.reduce((sum, o) => sum + o[1], 0);
+const games = parse(input);
 
-console.log(val);
+let sum = 0;
+for (let { id, maxCubes } of games) {
+	if (maxCubes.red <= 12 && maxCubes.green <= 13 && maxCubes.blue <= 14) {
+		sum += id;
+	}
+}
+
+console.log(sum);
