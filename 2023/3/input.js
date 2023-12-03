52c948c9ad2bc092cdaa45db76da46e6e277235f
romellem <romellem.git@gmail.com>
2023-12-02 23:17:45 -0600

    save, wrong answer

--- 2023/3/input.js
+++ 2023/3/input.js
@@ -1,11 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const input = fs
-	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
-	.toString()
-	.trim()
-	.split('\n');
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').toString().trim();
 
 module.exports = {
 	input,
--- 2023/3/part-one.js
+++ 2023/3/part-one.js
@@ -1,5 +1,58 @@
 const { input } = require('./input');
+const { InfiniteGrid } = require('./infinite-grid');
 
-let val = input;
+function getNums() {
+	let tempGrid = InfiniteGrid.split(input);
+	let in_num = false;
+	let num = '';
+	let numMap = new Map();
+	for (let y = 0; y < tempGrid.length; y++) {
+		for (let x = 0; x < tempGrid[y].length; x++) {
+			let v = tempGrid[y][x];
+			if (/\d/.test(v)) {
+				if (in_num) {
+					num += v;
+				} else {
+					in_num = true;
+					num = v;
+				}
+			} else {
+				if (in_num) {
+					// pop num
+					numMap.set(InfiniteGrid.toId(x, y), +num);
+					in_num = false;
+					num = '';
+				}
+			}
+		}
+	}
 
-console.log(val);
+	return numMap;
+}
+
+const nums = getNums();
+const grid = new InfiniteGrid({ load: input });
+
+let partNums = [];
+for (let [id, num] of nums) {
+	let coord = InfiniteGrid.toCoords(id);
+	let cells = String(num)
+		.split('')
+		.map((_, i) => [coord[0] + i, coord[1]]);
+	let neighbors = cells.map(([x, y]) => grid.neighbors(x, y, true));
+	if (
+		neighbors.some((group) => {
+			return [...group.values()].some(({ id, coord, value }) => /[^\.\d]/.test(value));
+		})
+	) {
+		partNums.push([id, num]);
+	}
+}
+
+// for (let [val, [x, y]] of symbols) {
+// 	const n = grid.neighbors(x, y, true);
+// 	if ([...n.values()].some(({ id, coord, value }) => /\d/.test(value))) {
+// 	}
+// }
+
+console.log(partNums.reduce((sum, a) => sum + a[1], 0));
