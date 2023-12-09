78bca421a0e21271902dcb1c70317619651c6849
romellem <romellem.git@gmail.com>
2023-12-09 15:23:15 -0500

    Refactors part one and part two
    Adds README with answers as well

--- 2023/4/part-one.js
+++ 2023/4/part-one.js
@@ -1,23 +1,27 @@
 const { input } = require('./input');
-// Card   1: 57 76 72 11  8 28 15 38 54 46 | 77 87 71 98 40  7 84 43 61 64  5 50 19 83 79 99 36 47  4 95 30 44 37 55 26
+
+function parseLine(line) {
+	const [, cardNum, winningCards, yourHand] = /^Card\s+(\d+): ([^\|]+) \| ([^\|]+)$/.exec(line);
+
+	return {
+		cardNum: parseInt(cardNum, 10),
+		winningCards: winningCards
+			.trim()
+			.split(' ') // Split on single space and remove empties via Boolean filter
+			.filter(Boolean)
+			.map((v) => parseInt(v, 10)),
+		yourHand: yourHand
+			.trim()
+			.split(' ')
+			.filter(Boolean)
+			.map((v) => parseInt(v, 10)),
+	};
+}
 
 let sum = 0;
 for (let line of input) {
-	let [, cardNum, winningCards, yourHand] = /^Card\s+(\d+): ([^\|]+) \| ([^\|]+)$/.exec(line);
-
-	winningCards = winningCards
-		.trim()
-		.split(' ')
-		.filter(Boolean)
-		.map((v) => +v);
-	yourHand = yourHand
-		.trim()
-		.split(' ')
-		.filter(Boolean)
-		.map((v) => +v);
-
-	let winningCardsSet = new Set(winningCards);
-	let yourHandSet = new Set(yourHand);
+	const { winningCards, yourHand } = parseLine(line);
+	const winningCardsSet = new Set(winningCards);
 
 	const haveWins = yourHand.filter((card) => winningCardsSet.has(card));
 
--- 2023/4/part-two.js
+++ 2023/4/part-two.js
@@ -1,41 +1,43 @@
 const { input } = require('./input');
-// Card   1: 57 76 72 11  8 28 15 38 54 46 | 77 87 71 98 40  7 84 43 61 64  5 50 19 83 79 99 36 47  4 95 30 44 37 55 26
 
-let copies = Array(input.length + 1).fill(1);
-copies[0] = 0;
+function parseLine(line) {
+	// @example `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53`
+	const [, cardNum, winningCards, yourHand] = /^Card\s+(\d+): ([^\|]+) \| ([^\|]+)$/.exec(line);
 
-let sum = 0;
+	return {
+		cardNum: parseInt(cardNum, 10),
+		winningCards: winningCards
+			.trim()
+			.split(' ') // Split on single space and remove empties via Boolean filter
+			.filter(Boolean)
+			.map((v) => parseInt(v, 10)),
+		yourHand: yourHand
+			.trim()
+			.split(' ')
+			.filter(Boolean)
+			.map((v) => parseInt(v, 10)),
+	};
+}
+
+// Use `cardNum - 1` as indices. Each card's count starts at 1 for its own self
+const copies = Array(input.length).fill(1);
+
 for (let line of input) {
-	let [, cardNum, winningCards, yourHand] = /^Card\s+(\d+): ([^\|]+) \| ([^\|]+)$/.exec(line);
-
-	cardNum = +cardNum;
-	winningCards = winningCards
-		.trim()
-		.split(' ')
-		.filter(Boolean)
-		.map((v) => +v);
-	yourHand = yourHand
-		.trim()
-		.split(' ')
-		.filter(Boolean)
-		.map((v) => +v);
-
-	let winningCardsSet = new Set(winningCards);
+	const { cardNum, winningCards, yourHand } = parseLine(line);
+	const winningCardsSet = new Set(winningCards);
 
 	const haveWins = yourHand.filter((card) => winningCardsSet.has(card));
-	const cardCopies = copies[cardNum];
+	const cardIndex = cardNum - 1;
+	const cardCopies = copies[cardIndex];
 
 	if (haveWins.length) {
-		let newCopies = Array(haveWins.length)
+		const newCopies = Array(haveWins.length)
 			.fill()
-			.map((_, i) => cardNum + i + 1);
+			.map((_, i) => cardIndex + 1 + i); // e.g. `cardNum + i`
 		for (let copy of newCopies) {
 			copies[copy] += cardCopies;
 		}
 	}
-
-	// const score = haveWins.length ? 2 ** (haveWins.length - 1) : 0;
-	// sum += score * cardCopies;
 }
 
 console.log(copies.reduce((a, b) => a + b, 0));
