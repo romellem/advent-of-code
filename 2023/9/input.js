8a63bb14ae026063651ec1e851309358bc54bbd7
romellem <romellem.git@gmail.com>
2023-12-09 12:48:42 -0500

    Working part one

--- 2023/9/input.js
+++ 2023/9/input.js
@@ -5,8 +5,17 @@ const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
-	.split('\n');
+	.split('\n')
+	.map((line) => line.split(' ').map((v) => parseInt(v, 10)));
+
+const sampleInput = fs
+	.readFileSync(path.join(__dirname, 'sample.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\n')
+	.map((line) => line.split(' ').map((v) => parseInt(v, 10)));
 
 module.exports = {
 	input,
+	sampleInput,
 };
