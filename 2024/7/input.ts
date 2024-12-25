bc02a112fd54beddd8331930ea54628c36c1fb25
romellem <romellem.git@gmail.com>
2024-12-25 16:47:07 -0600

    init 2024 day 7

---
+++ 2024/7/input.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import fs from 'fs';
+
+export const input: Array<{ result: number; numbers: number[] }> = fs
+	.readFileSync(path.join(import.meta.dirname, 'input.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\n')
+	.map((line: string) => {
+		const [result, numbers] = line.split(':');
+		return {
+			result: parseInt(result, 10),
+			numbers: numbers
+				.trim()
+				.split(' ')
+				.map((v) => parseInt(v, 10)),
+		};
+	});
