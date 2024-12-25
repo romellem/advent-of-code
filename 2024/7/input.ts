eaa2b24b5e6c86915589c0b47d834a93d659f913
romellem <romellem.git@gmail.com>
2024-12-25 17:32:28 -0600

    I think working part one

--- 2024/7/input.ts
+++ 2024/7/input.ts
@@ -16,3 +16,19 @@ export const input: Array<{ result: number; numbers: number[] }> = fs
 				.map((v) => parseInt(v, 10)),
 		};
 	});
+
+export const sampleInput: Array<{ result: number; numbers: number[] }> = fs
+	.readFileSync(path.join(import.meta.dirname, 'sample-input.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\n')
+	.map((line: string) => {
+		const [result, numbers] = line.split(':');
+		return {
+			result: parseInt(result, 10),
+			numbers: numbers
+				.trim()
+				.split(' ')
+				.map((v) => parseInt(v, 10)),
+		};
+	});
