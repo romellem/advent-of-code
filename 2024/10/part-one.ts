ee459276e137de2d9190e7e099af81ae46bca752
romellem <romellem.git@gmail.com>
2024-12-30 18:33:27 -0400

    Adjust and comment

--- 2024/10/part-one.ts
+++ 2024/10/part-one.ts
@@ -6,22 +6,22 @@ const hikingTrails = grid.findAll(0);
 
 function findTrailheads(id: GridId, grid: InfiniteGrid<number>) {
 	const findTrailheadsDFS = (
-		id: GridId,
 		currentPath: Array<GridId>,
 		allPaths: Array<Array<GridId>>
 	): Array<Array<GridId>> => {
-		const x = InfiniteGrid.toXCoord(id);
-		const y = InfiniteGrid.toYCoord(id);
+		const lastId = currentPath.at(-1)!;
+		const x = InfiniteGrid.toXCoord(lastId);
+		const y = InfiniteGrid.toYCoord(lastId);
 		const value = grid.get(x, y);
 
 		if (value === 9) {
 			// At the end of the trail, save it
-			allPaths.push([...currentPath, id]);
+			allPaths.push(currentPath);
 		} else {
 			for (let neighbor of grid.neighbors(x, y).values()) {
 				if (neighbor.value === value + 1) {
-					// Valid next step, recurse down this new path
-					findTrailheadsDFS(neighbor.id, [...currentPath, id], allPaths);
+					// Valid next step, recurse down this new path from that neighbor
+					findTrailheadsDFS([...currentPath, neighbor.id], allPaths);
 				}
 			}
 		}
@@ -29,13 +29,25 @@ function findTrailheads(id: GridId, grid: InfiniteGrid<number>) {
 		return allPaths;
 	};
 
-	const trailheads = findTrailheadsDFS(id, [], []);
+	// Start the walk from the `id`
+	const trailheads = findTrailheadsDFS([id], []);
 	return trailheads;
 }
 
 let trailheadSum = 0;
 for (let { id } of hikingTrails) {
 	const trailheads = findTrailheads(id, grid);
+	/**
+	 * > A trailhead's score is the number of 9-height positions reachable
+	 * > from that trailhead via a hiking trail.
+	 *
+	 * `trailheads` are all the full valid paths to the end of the trail.
+	 * However, for part one, we only care about how many _endings_ the trail
+	 * has, now how many ways there are to get to that end.
+	 *
+	 * So, pop off the ending coord (the `9` height cell) and count the
+	 * number of unique peaks we have to add to our sum.
+	 */
 	const uniquePeaks = new Set(trailheads.map((trail) => trail.at(-1)));
 	trailheadSum += uniquePeaks.size;
 }
