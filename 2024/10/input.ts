48b2847ff8622f477037e8ccdb3a80e6ff833092
romellem <romellem.git@gmail.com>
2024-12-30 17:07:14 -0400

    Init day 10

---
+++ 2024/10/input.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+import fs from 'fs';
+
+export const input = fs
+	.readFileSync(path.join(import.meta.dirname, 'input.txt'), 'utf8')
+	.toString()
+	.trim();
---
+++ 2024/10/part-one.ts
@@ -0,0 +1,2 @@
+import { input } from './input';
+import { InfiniteGrid } from './infinite-grid';
