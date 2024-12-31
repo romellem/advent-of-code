1d840085371ea4d3a1645832a46c59390db50adf
romellem <romellem.git@gmail.com>
2024-12-31 15:23:17 -0600

    Close, but moving disk doesn't leave free space behind
    Gah, I should just use a flat disk and calculate every time. Nuts!

--- 2024/9/part-two.ts
+++ 2024/9/part-two.ts
@@ -1,15 +1,14 @@
-import { input } from './input';
+import { sampleInput as input } from './input';
 
 type File = {
 	type: 'file';
-	size: number;
+	data: Array<number>;
 	index: number;
-	id: number;
 };
 
 type FreeSpace = {
 	type: 'free-space';
-	size: number;
+	data: Array<undefined>;
 	index: number;
 };
 
@@ -25,44 +24,38 @@ for (let i = 0; i < input.length; i++) {
 	if (isFile) {
 		disk.push({
 			type: 'file',
-			size: num,
+			data: Array(num).fill(id),
 			index: i,
-			id,
 		});
 	} else {
 		id++;
 		disk.push({
 			type: 'free-space',
-			size: num,
+			data: Array(num).fill(undefined),
 			index: i,
 		});
 	}
 }
 
-const sortFreeSpaceBySize = () => {
-	const freeSpaces = disk.filter((v) => v.type === 'free-space');
-	freeSpaces.sort((a, b) => {
-		if (a.size === b.size) {
-			// sort by index if size is the same
-			return a.index - b.index;
-		}
-		return a.size - b.size;
-	});
-
-	const freeSpaceMap = new Map<number, Array<FreeSpace>>();
-	for (let freeSpace of freeSpaces) {
-		const { size } = freeSpace;
-		if (!freeSpaceMap.has(size)) {
-			freeSpaceMap.set(size, []);
-		}
-		freeSpaceMap.get(size)!.push(freeSpace);
+const freeSpaces = disk.filter((v) => v.type === 'free-space');
+freeSpaces.sort((a, b) => {
+	if (a.data.length === b.data.length) {
+		// sort by index if size is the same
+		return a.index - b.index;
 	}
+	return a.data.length - b.data.length;
+});
 
-	return freeSpaceMap;
-};
+const freeSpaceMap = new Map<number, Array<FreeSpace>>();
+for (let freeSpace of freeSpaces) {
+	const size = freeSpace.data.length;
+	if (!freeSpaceMap.has(size)) {
+		freeSpaceMap.set(size, []);
+	}
+	freeSpaceMap.get(size)!.push(freeSpace);
+}
 
-const getFirstFreeSpaceThatCanFit = (size: number) => {
-	const freeSpaceMap = sortFreeSpaceBySize();
+function getFirstFreeSpaceThatCanFit(size: number): FreeSpace | undefined {
 	const possibleFreeSpaces: Array<FreeSpace> = [];
 	for (let [freeSize, freeSpaces] of freeSpaceMap) {
 		if (freeSize >= size) {
@@ -72,68 +65,77 @@ const getFirstFreeSpaceThatCanFit = (size: number) => {
 
 	possibleFreeSpaces.sort((a, b) => a.index - b.index);
 	return possibleFreeSpaces[0];
-};
-
-function move(file: File, freeSpace: FreeSpace) {
-	const fileIndex = disk.indexOf(file);
-	const freeSpaceIndex = disk.indexOf(freeSpace);
 }
 
-/**
- * Have four pointers, startA & startB and endA & endB
- * They span the first full empty block, and the last
- * full block. Each "B" pointer points to just _after_
- * the value, so I can easily slice that.
- *
- *                 endA    endB
- *     startA startB  |    |
- *          ↓ ↓       ↓    ↓
- *         0..111.....22222
- *
- * the start surpasses the end point.
- */
-let startA = disk.indexOf(undefined);
-let startB = startA;
-while (disk[startB] === undefined) {
-	startB++;
-}
-
-// Ahve `endB` but one past the end, so the full length (0 indices and all)
-let endB = disk.length;
-let endBValue = disk[endB - 1];
-let endA = endB;
-while (disk[endA] !== endBValue) {
-	endA--;
-}
-// We went just past the value in the above loop, so tick it forward 1 after
-endA++;
-
-const startSize = () => startB - startA;
-const endSize = () => endB - endA;
-
-while (startB < endA) {
-	if (disk[start] !== undefined) {
-		start++;
-		continue;
-	}
-	if (disk[end] === undefined) {
-		end--;
-		continue;
+function tryToMoveFileToFreeSpace(file: File) {
+	const freespace = getFirstFreeSpaceThatCanFit(file.data.length);
+	if (!freespace) {
+		return;
 	}
 
-	disk[start] = disk[end];
-	disk[end] = undefined;
+	// Remove file from the end
+	disk.splice(file.index, 1);
+
+	// Insert file before free space, potentially shifting all subsequent indices
+	disk.splice(freespace.index, 0, file);
+
+	// Resize freespace
+	const newFreespaceSize = freespace.data.length - file.data.length;
+	if (newFreespaceSize === 0) {
+		// Remove free space entirely, no need to update other objects' indices
+		freeSpaceMap.get(freespace.data.length)!.shift();
+		disk.splice(freespace.index + 1, 1);
+	} else {
+		// Remove free space from current size, it will change
+		freeSpaceMap.get(freespace.data.length)!.shift();
+		freespace.data.length = newFreespaceSize;
+
+		// Update indices of all other objects, include currently resize freespace
+		for (let i = freespace.index; i < disk.length; i++) {
+			disk[i].index++;
+		}
+
+		// Update lookup map
+		if (!freeSpaceMap.has(newFreespaceSize)) {
+			freeSpaceMap.set(newFreespaceSize, []);
+		}
+
+		freeSpaceMap.get(newFreespaceSize)!.push(freespace);
+
+		// Heap
+		freeSpaceMap.get(newFreespaceSize)!.sort((a, b) => a.index - b.index);
+	}
+}
+
+// We update the `disk` when moving files, so use a different list that is stable
+const files = disk.filter((v) => v.type === 'file');
+
+console.log(
+	disk
+		.flatMap((v) => v.data)
+		.map((v) => v ?? '.')
+		.join('')
+);
+for (let i = files.length - 1; i >= 0; i--) {
+	const file = files[i];
+	tryToMoveFileToFreeSpace(file);
+	console.log(
+		disk
+			.flatMap((v) => v.data)
+			.map((v) => v ?? '.')
+			.join('')
+	);
 }
 
 // Compute checksum
-let checksum = 0;
-for (let i = 0; i < disk.length; i++) {
-	let block = disk[i];
-	if (block === undefined) {
-		break;
-	}
+// let checksum = 0;
+// for (let i = 0; i < flatDisk.length; i++) {
+// 	let block = flatDisk[i];
+// 	if (block === undefined) {
+// 		continue;
+// 	}
 
-	checksum += block * i;
-}
+// 	checksum += block * i;
+// }
 
-console.log(checksum);
+// console.log(checksum);
