1bf981685e7d95c3bd229aea8e62c2abc12a05f9
romellem <romellem.git@gmail.com>
2025-01-02 19:59:29 -0600

    FINALLY

--- 2024/9/part-two.ts
+++ 2024/9/part-two.ts
@@ -1,180 +1,89 @@
 import { input } from './input';
-const totalDiskSize = input.reduce((acc, v) => acc + v, 0);
-import util from 'util';
-const blue = (s: string) => util.format('\x1b[34m%s\x1b[0m', s);
 
-const disk: Array<number | undefined> = Array(totalDiskSize).fill(undefined);
+type File = {
+	id: number;
+	start: number;
+	end: number;
+	size: number;
+};
+type Freespace = {
+	start: number;
+	end: number;
+	size: number;
+};
 
-// Fill the disk
+const freespace: Array<Freespace> = [];
+const files: Array<File> = [];
+
+// Fill the disk, but keep track of freespace and files separately
 let id = 0;
 let index = 0;
 for (let i = 0; i < input.length; i++) {
 	const num = input[i];
 
 	const isFile = i % 2 === 0;
+	id += isFile ? 0 : 1;
+
+	if (num === 0) {
+		continue;
+	}
 
 	if (isFile) {
-		for (let n = 0; n < num; n++) {
-			disk[index + n] = id;
-		}
+		files.push({
+			id,
+			start: index,
+			end: index + num,
+			size: num,
+		});
 	} else {
-		// Free space
-		id++;
+		freespace.push({
+			start: index,
+			end: index + num,
+			size: num,
+		});
 	}
 
 	index += num;
 }
 
-let start = disk.indexOf(undefined);
-let end = disk.length;
-
-function findLastFullFile(): undefined | [number, number] {
-	let beginning = end - 1;
-	if (start > beginning) {
-		return undefined;
-	}
-
-	let fileId = disk[beginning];
-	while (disk[beginning] === fileId) {
-		beginning--;
-	}
-	beginning++;
-
-	if (start > beginning) {
-		return undefined;
-	}
-
-	return [beginning, end];
-}
-
-function getFreeSpaceBlocks(): Map<number, number> {
-	if (start > end) {
-		return new Map();
-	}
-
-	// Array of indices for free space
-	let freespace: Array<number> = [];
-	for (let i = start; i < end; i++) {
-		if (disk[i] === undefined) {
-			freespace.push(i);
+/**
+ * Unoptimized, loop free space in order until we find one that is
+ * at least the size of the file we care about.
+ */
+function findFreeSpace(file: File): Freespace | undefined {
+	for (let block of freespace) {
+		if (block.start < file.start && block.size >= file.size) {
+			return block;
 		}
 	}
 
-	if (freespace.length === 0) {
-		return new Map();
-	} else if (freespace.length === 1) {
-		return new Map([[1, freespace[0]]]);
-	}
-
-	// Key is the size, and value is the start index of the block
-	let blocks = new Map<number, number>();
-	let size = 0;
-	for (let i = 0; i < freespace.length; i++) {
-		size++;
-		let current = freespace[i];
-		let next: number | undefined = freespace[i + 1];
-
-		// We'll end this case for the last block as well (e.g., when `next` is `undefined`)
-		if (next !== current + 1) {
-			/**
-			 * Let's say we have a disk that looks like
-			 *
-			 *   index: 0123456789
-			 *    disk: 11...22..3
-			 *
-			 * So we'd have freespace array that looks like
-			 *
-			 *   [2,3,4,7,8]
-			 *
-			 * In this example, the first time we his this `else` block,
-			 * `current = 4`, and `next = 7`. So, we want to record a block size
-			 * of 3, starting at index 2.
-			 *
-			 * To calculate that, take the fact that `current` is the last index of the block,
-			 * and subtract the size to move over the just before the start of the block.
-			 * Add 1 to bump the pointer to the actual start point.
-			 *
-			 * The math in this example then is `4 - 3 + 1` = 2, which is the start of the 3 sized block!
-			 */
-			for (let j = 0; j < size; j++) {
-				if (!blocks.has(size - j)) {
-					blocks.set(size - j, current - size + 1);
-				}
-			}
-
-			size = 0;
-		}
-	}
-
-	return blocks;
+	return undefined;
 }
 
-let q = 0;
-
-// Move our file IDs to chunks of free space!
-while (true) {
-	if (q++ > 50) {
-		break;
-	}
-	let startStr = ' '.repeat(start > 0 ? start : 0) + 's';
-	let endStr = ' '.repeat(end - start - 1 > 0 ? end - start - 1 : 0) + 'e';
-
-	// console.log(startStr + endStr);
-	// console.log(
-	// 	blue(
-	// 		Array(disk.length)
-	// 			.fill(undefined)
-	// 			.map((_, i) => i % 10)
-	// 			.join('')
-	// 	)
-	// );
-	// console.log(disk.map((v) => v ?? '.').join(''));
-
-	const fileParts = findLastFullFile();
-	if (fileParts === undefined) {
-		break;
+function partialChecksumForFile(file: File): number {
+	let partialChecksum = 0;
+	for (let i = file.start; i < file.end; i++) {
+		partialChecksum += i * file.id;
 	}
 
-	// console.log(fileParts.join(','), disk.slice(...fileParts));
-
-	const [startFile, endFile] = fileParts;
-	const fileId = disk[startFile];
-	const size = endFile - startFile;
-	const freespace = getFreeSpaceBlocks();
-
-	if (freespace.has(size)) {
-		const startFree = freespace.get(size)!;
-		for (let i = 0; i < size; i++) {
-			let freeIndex = startFree + i;
-			let fileIndex = startFile + i;
-
-			disk[freeIndex] = fileId;
-			disk[fileIndex] = undefined;
-		}
-
-		end = startFile;
-		start = disk.indexOf(undefined);
-	} else {
-		// Skip this file, couldn't move it
-		end = startFile;
-	}
-
-	while (disk[end - 1] === undefined) {
-		end--;
-	}
+	return partialChecksum;
 }
 
-// Compute checksum
 let checksum = 0;
-for (let i = 0; i < disk.length; i++) {
-	let block = disk[i];
-	if (block === undefined) {
-		continue;
+// Compute checksum and move files at the same time
+for (let i = files.length - 1; i >= 0; i--) {
+	let endFile = files[i];
+	let freeSpace = findFreeSpace(endFile);
+
+	if (freeSpace !== undefined) {
+		endFile.start = freeSpace.start;
+		endFile.end = freeSpace.start + endFile.size;
+
+		freeSpace.start = endFile.end;
+		freeSpace.size -= endFile.size;
 	}
 
-	checksum += block * i;
+	checksum += partialChecksumForFile(endFile);
 }
 
 console.log(checksum);
-
-// 15618464832081 too high
