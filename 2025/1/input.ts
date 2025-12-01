fb3df47ee73d1bc1b2ae625570a4acbae9d5603c
romellem <romellem.git@gmail.com>
2025-12-01 16:53:13 -0600

    First attempt, whoops, wrap the top end

--- 2025/1/input.ts
+++ 2025/1/input.ts
@@ -1,4 +1,20 @@
 import path from 'path';
 import fs from 'fs';
 
-export const input: Array<unknown> = [];
+export type Direction = 'L' | 'R';
+
+export type InputAction = {
+	direction: Direction;
+	degree: number;
+};
+
+export const input: Array<InputAction> = fs
+	.readFileSync(path.join(import.meta.dirname, 'input.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\n')
+	.map((line: string) => {
+		const direction = line.at(0) as Direction;
+		const degree = parseInt(line.slice(1), 10);
+		return { direction, degree };
+	});
--- 2025/1/part-one.ts
+++ 2025/1/part-one.ts
@@ -1,1 +1,52 @@
-import { input } from './input';
+import { input, type Direction } from './input';
+
+type RotateOptions = {
+	currentNumber: number;
+	direction: Direction;
+	degree: number;
+	min?: number;
+	max?: number;
+};
+
+function rotate({ currentNumber, direction, degree, min = 0, max = 100 }: RotateOptions) {
+	// Rotating left is subtracting the degree
+	if (direction === 'L') {
+		degree *= -1;
+	}
+
+	const modulus = max - min;
+
+	// Remove any full turns from input
+	degree %= modulus;
+
+	// Rotate current number
+	let newNumber = currentNumber + degree;
+
+	// Map negative values to positive ones
+	if (newNumber < min) {
+		newNumber = max + newNumber;
+	}
+
+	return newNumber;
+}
+
+// Dial starts at 50
+const dialValues = [50];
+
+for (let { direction, degree } of input) {
+	const currentNumber = dialValues.at(-1)!;
+	const nextNumber = rotate({
+		currentNumber,
+		direction,
+		degree,
+	});
+
+	dialValues.push(nextNumber);
+}
+
+// > The actual password is the number of times the dial is left
+// > pointing at 0 after any rotation in the sequence.
+const dialZeroes = dialValues.filter((v) => v === 0);
+const numZeroes = dialZeroes.length;
+
+console.log('Part 1:', numZeroes);
