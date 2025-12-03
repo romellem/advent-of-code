5ac7d2530749604b54bb508aafc32b7b28223ccd
romellem <romellem.git@gmail.com>
2025-12-03 09:11:18 -0600

    First digit can't be the last one

--- 2025/3/part-one.ts
+++ 2025/3/part-one.ts
@@ -2,10 +2,16 @@ import { sampleInput as input } from './input';
 
 console.log(input);
 
-function findMaxInRow(row: number[], fromIndex: number = 0): { index: number; value: number } {
+function findMaxInRow(
+	row: number[],
+	isFirstDigit: boolean,
+	fromIndex: number = 0
+): { index: number; value: number } {
 	let maxValue = -1;
 	let maxIndex = -1;
-	for (let i = fromIndex; i < row.length; i++) {
+
+	const end = isFirstDigit ? row.length - 1 : row.length;
+	for (let i = fromIndex; i < end; i++) {
 		const value = row[i];
 		if (value > maxValue) {
 			maxValue = value;
@@ -17,14 +23,13 @@ function findMaxInRow(row: number[], fromIndex: number = 0): { index: number; va
 }
 
 function findMaxBatteryPair(row: number[]) {
-	let firstBattery = findMaxInRow(row);
-	let secondBattery = findMaxInRow(row, firstBattery.index + 1);
+	let firstBattery = findMaxInRow(row, true);
+	let secondBattery = findMaxInRow(row, false, firstBattery.index + 1);
 
 	return 10 * firstBattery.value + secondBattery.value;
 }
 
 const batteries = input.map((row) => findMaxBatteryPair(row));
-console.log(batteries);
 const sum = batteries.reduce((a, b) => a + b);
 
 console.log('Part 1:', sum);
