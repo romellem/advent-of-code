fe3336f8815a77f69df9b61d6629a312e8f07553
romellem <romellem.git@gmail.com>
2025-12-03 10:04:17 -0600

    Comment

--- 2025/3/part-two.ts
+++ 2025/3/part-two.ts
@@ -1,4 +1,4 @@
-import { sampleInput as input } from './input';
+import { input } from './input';
 
 type Battery = { index: number; value: number };
 
@@ -19,24 +19,32 @@ function findMaxInRow(row: number[], fromIndex: number, toIndex: number): Batter
 
 function findMaxBatteryTuple(row: number[], size: number) {
 	const batteryCells: Array<Battery> = [];
-	for (let i = 0; i < size; i++) {
-		const currentBatteryCount = i + 1;
-		const previousBatteryIndex = batteryCells.at(-1)?.index ?? -1;
-		const batteryCell = findMaxInRow(
-			row,
-			previousBatteryIndex + 1,
-			row.length - size + currentBatteryCount
-		);
+
+	for (let count = 1; count <= size; count++) {
+		/**
+		 * If we have no batteries, start searching from beginning.
+		 * Otherwise search from 1 index after the last battery found.
+		 */
+		const searchFromIndex = batteryCells.length === 0 ? 0 : batteryCells.at(-1)!.index + 1;
+
+		/**
+		 * Don't search past the number of batteries left to find.
+		 * E.g. if there is 1 battery left in our count, only search until the second to last index
+		 */
+		const batteriesLeft = size - count;
+		const searchToIndex = row.length - batteriesLeft;
+
+		const batteryCell = findMaxInRow(row, searchFromIndex, searchToIndex);
 
 		batteryCells.push(batteryCell);
 	}
 
-	const batteryStr = batteryCells.map(({ value }) => value.toString()).join('');
-	return parseInt(batteryStr, 0);
+	// Concat all digits together and return a single number
+	const batteryStr = batteryCells.map(({ value }) => value).join('');
+	return parseInt(batteryStr, 10);
 }
 
 const batteries = input.map((row) => findMaxBatteryTuple(row, 12));
-console.log(batteries);
 const sum = batteries.reduce((a, b) => a + b);
 
 console.log('Part 2:', sum);
