2ef93f92b6f38ea5b5aea4ff153993b7c2d8b5c2
romellem <romellem.git@gmail.com>
2025-12-02 21:15:40 -0600

    Init day 2, 2025

---
+++ 2025/2/input.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import fs from 'fs';
+
+export type IdRange = {
+	first: number;
+	last: number;
+};
+
+function parse(filename: string): Array<IdRange> {
+	return fs
+		.readFileSync(path.join(import.meta.dirname, filename), 'utf8')
+		.toString()
+		.trim()
+		.split(',')
+		.map((numberPairs: string) => {
+			const [firstStr, lastStr] = numberPairs.split(',');
+			const first = parseInt(firstStr, 10);
+			const last = parseInt(lastStr, 10);
+			return { first, last };
+		});
+}
+
+export const input = parse('input.txt');
+export const sampleInput = parse('sample-input.txt');
