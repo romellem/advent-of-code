8171fc476b00a368fd21164ec0e4a58ac85e0e1f
romellem <romellem.git@gmail.com>
2025-12-03 09:11:00 -0600

    First attempt, doesn't work

--- 2025/3/part-one.ts
+++ 2025/3/part-one.ts
@@ -1,1 +1,30 @@
-import { input } from './input';
+import { sampleInput as input } from './input';
+
+console.log(input);
+
+function findMaxInRow(row: number[], fromIndex: number = 0): { index: number; value: number } {
+	let maxValue = -1;
+	let maxIndex = -1;
+	for (let i = fromIndex; i < row.length; i++) {
+		const value = row[i];
+		if (value > maxValue) {
+			maxValue = value;
+			maxIndex = i;
+		}
+	}
+
+	return { index: maxIndex, value: maxValue };
+}
+
+function findMaxBatteryPair(row: number[]) {
+	let firstBattery = findMaxInRow(row);
+	let secondBattery = findMaxInRow(row, firstBattery.index + 1);
+
+	return 10 * firstBattery.value + secondBattery.value;
+}
+
+const batteries = input.map((row) => findMaxBatteryPair(row));
+console.log(batteries);
+const sum = batteries.reduce((a, b) => a + b);
+
+console.log('Part 1:', sum);
