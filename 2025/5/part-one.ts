b6149a5d6118374e667e865a48d378f4dbf7c764
romellem <romellem.git@gmail.com>
2025-12-04 23:43:46 -0600

    Working part one

--- 2025/5/part-one.ts
+++ 2025/5/part-one.ts
@@ -1,1 +1,16 @@
 import { input } from './input';
+
+const { ranges, ingredients } = input;
+
+let freshCount = 0;
+for (let ingredient of ingredients) {
+	const inRange = ranges.some((range) => {
+		return ingredient >= range.low && ingredient <= range.high;
+	});
+
+	if (inRange) {
+		freshCount++;
+	}
+}
+
+console.log('Part 1:', freshCount);
