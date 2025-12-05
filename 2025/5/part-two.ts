60999562413f53c0ab8107ea4565fb3406c43ac3
romellem <romellem.git@gmail.com>
2025-12-05 00:16:16 -0600

    Working part two
    Easy peasy, sorting ahead of time is the trick

--- 2025/5/part-two.ts
+++ 2025/5/part-two.ts
@@ -1,1 +1,27 @@
 import { input } from './input';
+
+const { ranges: unsortedRanges, ingredients } = input;
+const ranges = unsortedRanges.sort((a, z) => a.low - z.low);
+
+type Range = (typeof ranges)[number];
+
+// Add in first range to ensure easier looping
+// since we won't have to deal with the empty array edge case
+const compactRanges: Array<Range> = [];
+
+for (let i = 0; i < ranges.length; i++) {
+	const nextRange = ranges[i];
+	const previousRange = compactRanges.at(-1);
+
+	if (previousRange && nextRange.low <= previousRange.high) {
+		previousRange.high = Math.max(nextRange.high, previousRange.high);
+	} else {
+		// No overlap, insert new range
+		compactRanges.push(nextRange);
+	}
+}
+
+const counts = compactRanges.map(({ low, high }) => high - low + 1);
+const sumOfCounts = counts.reduce((a, b) => a + b, 0);
+
+console.log('Part 2:', sumOfCounts);
